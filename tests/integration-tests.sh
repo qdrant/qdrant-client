8187c3bda36ce699b843891d0400245bdead8784
Andrey Vasnetsov <andrey@vasnetsov.com>
2022-02-11 15:07:54 +0100

    add integration test pipeline for CI (#20)
    * add integration test pipeline for CI
    
    * chmod +x
    
    * upd poetry install

---
+++ tests/integration-tests.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -ex
+
+function stop_docker()
+{
+  echo "stopping qdrant_test"
+  docker stop qdrant_test
+}
+
+# Ensure current path is project root
+cd "$(dirname "$0")/../"
+
+QDRANT_VERSION='v0.5.0'
+
+QDRANT_HOST='localhost:6333'
+
+docker run -d --rm --network=host --name qdrant_test qdrant/qdrant:${QDRANT_VERSION}
+
+trap stop_docker SIGINT
+trap stop_docker ERR
+
+until $(curl --output /dev/null --silent --get --fail http://$QDRANT_HOST/collections); do
+  printf 'waiting for server to start...'
+  sleep 5
+done
+
+pytest
+
+echo "Ok, that is enough"
+
+stop_docker
