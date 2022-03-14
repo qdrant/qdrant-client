6f61432d843a0d6980dcf07a182be50e2381369b
Andrey Vasnetsov <andrey@vasnetsov.com>
2022-03-14 13:21:21 +0100

    clients update instruction

---
+++ tools/generate_rest_client.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+PROJECT_ROOT="$(pwd)/$(dirname "$0")/../"
+
+cd $(mktemp -d)
+
+git clone --sparse --filter=blob:none --depth=1 git@github.com:qdrant/qdrant.git
+cd qdrant
+git sparse-checkout add docs/redoc/master
+
+OPENAPI_PATH="$(pwd)/docs/redoc/master/openapi.json"
+
+cd $(mktemp -d)
+
+git clone git@github.com:qdrant/pydantic_openapi_v3.git
+cd pydantic_openapi_v3
+
+poetry install
+
+cp $OPENAPI_PATH openapi-qdrant.yaml
+
+PATH_TO_QDRANT_CLIENT=$PROJECT_ROOT
+
+INPUT_YAML=openapi-qdrant.yaml IMPORT_NAME="qdrant_client.http" PACKAGE_NAME=qdrant_openapi_client bash -x scripts/model_data_generator.sh
+
+rm -rf ${PATH_TO_QDRANT_CLIENT}/qdrant_client/http/
+mv scripts/output/qdrant_openapi_client ${PATH_TO_QDRANT_CLIENT}/qdrant_client/http/
