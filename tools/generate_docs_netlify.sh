301c52ce9cdefa8d5969a17f76b2461eb5f99901
Andrey Vasnetsov <andrey@vasnetsov.com>
2022-04-12 22:52:01 +0200

    fix netlify deploy

--- tools/generate_docs_netlify.sh
+++ tools/generate_docs_netlify.sh
@@ -5,6 +5,8 @@ set -xe
 # Ensure current path is project root
 cd "$(dirname "$0")/../"
 
+pip install --no-deps qdrant-client
+
 pip install sphinx>=4.4.0
 pip install "git+https://github.com/qdrant/qdrant_sphinx_theme.git@master#egg=qdrant-sphinx-theme"
 
