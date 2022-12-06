b97f32b94780a473c2df828450ca1fc8eba86cd6
Guillaume Robin <guillaume@inarix.com>
2022-12-06 15:33:44 +0100

    Add prefix argument to QdrantClient constructor

--- qdrant_client/qdrant_client.py
+++ qdrant_client/qdrant_client.py
@@ -59,11 +59,16 @@ class QdrantClient:
                  prefer_grpc=False,
                  https=None,
                  api_key=None,
+                 prefix=None,
                  **kwargs):
         self._prefer_grpc = prefer_grpc
         self._grpc_port = grpc_port
         self._host = host
         self._port = port
+        self._prefix = prefix
+
+        if prefix is not None and len(prefix) > 0 and prefix[0] != '/':
+            self._prefix = f"/{prefix}"
 
         self._https = https
         self._api_key = api_key
@@ -89,7 +94,7 @@ class QdrantClient:
             self._rest_headers['api-key'] = api_key
             self._grpc_headers.append(('api-key', api_key))
 
-        self.rest_uri = f"http{'s' if self._https else ''}://{host}:{port}"
+        self.rest_uri = f"http{'s' if self._https else ''}://{host}:{port}{self._prefix if self._prefix is not None else ''}"
         self._rest_args = {
             "headers": self._rest_headers,
             "http2": http2,
