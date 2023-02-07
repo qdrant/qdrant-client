06e9189d012e74523343067f28c2ede030881de6
George Panchuk <george.panchuk@qdrant.tech>
2023-02-07 16:21:37 +0400

    fix: update models import for pylance

--- qdrant_client/http/__init__.py
+++ qdrant_client/http/__init__.py
@@ -6,7 +6,7 @@ from qdrant_client.http.api_client import (  # noqa F401
     AsyncApis as AsyncApis,
     SyncApis as SyncApis,
 )
-from qdrant_client.http.models import models
+from qdrant_client.http.models import models as models
 
 for model in inspect.getmembers(models, inspect.isclass):
     if model[1].__module__ == "qdrant_client.http.models.models":
