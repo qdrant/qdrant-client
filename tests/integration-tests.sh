6d2a230b582f2302a5211d48b48badc59360a11b
Luis Cossío <luis.cossio@outlook.com>
2023-05-10 09:26:53 -0400

    amend: replace hardcoded grpc port

--- tests/integration-tests.sh
+++ tests/integration-tests.sh
@@ -21,7 +21,7 @@ QDRANT_HOST=localhost:${REST_PORT}
 docker run -d --rm \
            -p ${REST_PORT}:${REST_PORT} \
            -p ${GRPC_PORT}:${GRPC_PORT} \
-           -e QDRANT__SERVICE__GRPC_PORT="6334" \
+           -e QDRANT__SERVICE__GRPC_PORT=${GRPC_PORT} \
            --name qdrant_test qdrant/qdrant:${QDRANT_VERSION}
 
 trap stop_docker SIGINT
