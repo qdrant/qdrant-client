a7d54b5aa83b00c2960e902a3dbe010f9d2bd8c5
George Panchuk <george.panchuk@qdrant.tech>
2023-06-16 15:20:29 +0400

    fix: update type

--- qdrant_client/client_base.py
+++ qdrant_client/client_base.py
@@ -256,7 +256,9 @@ class QdrantBase:
     def upload_collection(
         self,
         collection_name: str,
-        vectors: Union[types.NumpyArray, Iterable[types.VectorStruct]],
+        vectors: Union[
+            Dict[str, types.NumpyArray], types.NumpyArray, Iterable[types.VectorStruct]
+        ],
         payload: Optional[Iterable[Dict[Any, Any]]] = None,
         ids: Optional[Iterable[types.PointId]] = None,
         **kwargs: Any,
