338445af5036d8baab5b72364c7cd26a1fcd52a2
George Panchuk <george.panchuk@qdrant.tech>
2023-07-15 02:56:54 +0400

    new: add types to pydantic_compat

--- qdrant_client/_pydantic_compat.py
+++ qdrant_client/_pydantic_compat.py
@@ -1,31 +1,34 @@
+from typing import Any, Dict, Type
+
+from pydantic import BaseModel
 from pydantic.version import VERSION as pydantic_version
 
 PYDANTIC_V2 = pydantic_version.startswith("2.")
 
 
-def update_forward_refs(model_class, *args, **kwargs):
+def update_forward_refs(model_class: Type[BaseModel], *args: Any, **kwargs: Any) -> None:
     if PYDANTIC_V2:
         model_class.model_rebuild(*args, **kwargs)
     else:
         model_class.update_forward_refs(*args, **kwargs)
 
 
-def construct(model, *args, **kwargs):
+def construct(model_class: Type[BaseModel], *args: Any, **kwargs: Any) -> BaseModel:
     if PYDANTIC_V2:
-        return model.model_construct(*args, **kwargs)
+        return model_class.model_construct(*args, **kwargs)
     else:
-        return model.construct(*args, **kwargs)
+        return model_class.construct(*args, **kwargs)
 
 
-def to_dict(model, *args, **kwargs):
+def to_dict(model: BaseModel, *args: Any, **kwargs: Any) -> Dict[Any, Any]:
     if PYDANTIC_V2:
         return model.model_dump(*args, **kwargs)
     else:
         return model.dict(*args, **kwargs)
 
 
-def to_json(model, *args, **kwargs):
+def to_json(model: BaseModel, *args: Any, **kwargs: Any) -> str:
     if PYDANTIC_V2:
-        return model.model_dump(*args, **kwargs)
-    else:
         return model.model_dump_json(*args, **kwargs)
+    else:
+        return model.json()
