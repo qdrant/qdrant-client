5ef5e19783f84fb6e54fc851a1a8dff5eb469591
George Panchuk <george.panchuk@qdrant.tech>
2023-08-06 00:18:22 +0200

    new: update auto-generated code

--- qdrant_client/http/models/models.py
+++ qdrant_client/http/models/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from enum import Enum
-from typing import Any, Dict, List, Optional, Union, Literal
+from typing import Any, Dict, List, Literal, Optional, Union
 
 from pydantic import BaseModel, Field
 from pydantic.types import StrictBool, StrictFloat, StrictInt, StrictStr
