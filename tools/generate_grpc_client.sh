395321d8c6eeb496a50e78e986570c873aa8b216
generall <andrey@vasnetsov.com>
2023-08-21 13:55:16 +0200

    init dev commit - do not cherry-pick

--- tools/generate_grpc_client.sh
+++ tools/generate_grpc_client.sh
@@ -6,7 +6,7 @@ PROJECT_ROOT="$(pwd)/$(dirname "$0")/../"
 
 cd $(mktemp -d)
 
-git clone --sparse --filter=blob:none --depth=1 git@github.com:qdrant/qdrant.git
+git clone --sparse --filter=blob:none --depth=1 -b dev git@github.com:qdrant/qdrant.git
 cd qdrant
 git sparse-checkout add lib/api/src/grpc/proto
 
