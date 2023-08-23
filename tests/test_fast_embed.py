a26a07769c2d42446dcbba7648e39c9d2654a42a
Nirant <NirantK@users.noreply.github.com>
2023-08-23 18:20:09 +0530

    Fixes for test, type errors (#266)
    * * fix(qdrant_fastembed.py): change method name from encode to embed in QdrantFastembedMixin class
    * fix(qdrant_fastembed.py): change method name from encode to embed
    
    * * fix(qdrant_fastembed.py): change DEFAULT_EMBEDDING_MODEL from "sentence-transformers/all-MiniLM-L6-v2" to "BAAI/bge-small-en"
    
    * * fix(qdrant_fastembed.py): add support for different embedding types in QdrantFastembedMixin
    * feat(qdrant_fastembed.py): add embed_type parameter to embed method in QdrantFastembedMixin
    
    * * fix(qdrant_fastembed.py): change vector field name from "text-{model_name}" to "fast-{model_name}"
    
    * * fix(qdrant_fastembed.py): add comments to clarify code intent
    
    * * refactor(qdrant_fastembed.py): remove unused import 'Sequence' from typing module
    
    * * test(test_fast_embed.py): refactor test_add_without_query function to simplify parameter handling and remove unnecessary checks and assignments
    
    * * fix(qdrant_fastembed.py): fix type error in NamedVector Query
    
    * * fix(qdrant_fastembed.py): change method name from `embed` to `query_embed` in QdrantFastembedMixin class
    * fix(qdrant_fastembed.py): fix query_vectors assignment in `query` method of QdrantFastembedMixin class

--- tests/test_fast_embed.py
+++ tests/test_fast_embed.py
@@ -6,31 +6,19 @@ from qdrant_client import QdrantClient
 
 
 def test_add_without_query(
-    local_client: QdrantClient = None,
+    local_client: QdrantClient = QdrantClient(":memory:"),
     collection_name: str = "demo_collection",
-    docs: Dict[str, List[Union[str, int, Any]]] = None,
-):
-    if local_client is None:
-        local_client = QdrantClient(":memory:")
-
-    if docs is None:
-        docs = {
-            "documents": [
+    docs: List[str] = [
                 "Qdrant has Langchain integrations",
                 "Qdrant also has Llama Index integrations",
             ],
-            "metadatas": [{"source": "Langchain-docs"}, {"source": "LlamaIndex-docs"}],
-            "ids": [42, 2],
-        }
-
+):
     if not local_client._is_fastembed_installed:
         pytest.skip("FastEmbed is not installed, skipping test")
 
     local_client.add(
         collection_name=collection_name,
-        documents=docs["documents"],
-        metadata=docs["metadatas"],
-        ids=docs["ids"],
+        documents=docs
     )
     assert local_client.count(collection_name).count == 2
 
