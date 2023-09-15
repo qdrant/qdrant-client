70c20d2f6d213fcda151961de31d6f11347e5a4f
George Panchuk <george.panchuk@qdrant.tech>
2023-09-15 17:54:41 +0400

    new: manually implemented async qdrant client

---
+++ qdrant_client/async_qdrant_fastembed.py
@@ -0,0 +1,345 @@
+import uuid
+from typing import Any, Dict, Iterable, List, Optional, Tuple, Union
+
+from pydantic import BaseModel
+
+from qdrant_client.async_client_base import AsyncQdrantBase
+from qdrant_client.conversions import common_types as types
+from qdrant_client.http import models
+from qdrant_client.uploader.uploader import iter_batch
+
+try:
+    from fastembed.embedding import DefaultEmbedding
+except ImportError:
+    DefaultEmbedding = None
+
+SUPPORTED_EMBEDDING_MODELS: Dict[str, Tuple[int, models.Distance]] = {
+    "BAAI/bge-base-en": (768, models.Distance.COSINE),
+    "sentence-transformers/all-MiniLM-L6-v2": (384, models.Distance.COSINE),
+    "BAAI/bge-small-en": (384, models.Distance.COSINE),
+}
+
+
+class QueryResponse(BaseModel, extra="forbid"):  # type: ignore
+    id: Union[str, int]
+    embedding: Optional[List[float]]
+    metadata: Dict[str, Any]
+    document: str
+    score: float
+
+
+class AsyncQdrantFastembedMixin(AsyncQdrantBase):
+    DEFAULT_EMBEDDING_MODEL = "BAAI/bge-small-en"
+
+    embedding_models: Dict[str, "DefaultEmbedding"] = {}
+
+    def __init__(self, **kwargs: Any):
+        self.embedding_model_name = self.DEFAULT_EMBEDDING_MODEL
+        super().__init__(**kwargs)
+
+    def set_model(self, embedding_model_name: str) -> None:
+        """
+        Set embedding model to use for encoding documents and queries.
+        Args:
+            embedding_model_name: One of the supported embedding models. See `SUPPORTED_EMBEDDING_MODELS` for details.
+
+        Raises:
+            ValueError: If embedding model is not supported.
+            ImportError: If fastembed is not installed.
+
+        Returns:
+            None
+        """
+        self._get_or_init_model(model_name=embedding_model_name)
+        self.embedding_model_name = embedding_model_name
+
+    @staticmethod
+    def _import_fastembed() -> None:
+        try:
+            from fastembed.embedding import DefaultEmbedding
+        except ImportError:
+            # If it's not, ask the user to install it
+            raise ImportError(
+                "fastembed is not installed."
+                " Please install it to enable fast vector indexing with `pip install fastembed`."
+            )
+
+    @classmethod
+    def _get_model_params(cls, model_name: str) -> Tuple[int, models.Distance]:
+        if model_name not in SUPPORTED_EMBEDDING_MODELS:
+            raise ValueError(
+                f"Unsupported embedding model: {model_name}. Supported models: {SUPPORTED_EMBEDDING_MODELS}"
+            )
+
+        return SUPPORTED_EMBEDDING_MODELS[model_name]
+
+    @classmethod
+    def _get_or_init_model(
+        cls, model_name: str
+    ) -> "DefaultEmbedding":  # -> Embedding: # noqa: F821
+        if model_name in cls.embedding_models:
+            return cls.embedding_models[model_name]
+
+        if model_name not in SUPPORTED_EMBEDDING_MODELS:
+            raise ValueError(
+                f"Unsupported embedding model: {model_name}. Supported models: {SUPPORTED_EMBEDDING_MODELS}"
+            )
+
+        cls._import_fastembed()
+
+        cls.embedding_models[model_name] = DefaultEmbedding(model_name=model_name)
+        return cls.embedding_models[model_name]
+
+    def _embed_documents(
+        self,
+        documents: List[str],
+        embedding_model_name: str = DEFAULT_EMBEDDING_MODEL,
+        batch_size: int = 32,
+        embed_type: str = "default",
+    ) -> Iterable[List[float]]:
+        embedding_model = self._get_or_init_model(model_name=embedding_model_name)
+        for batch_docs in iter_batch(documents, batch_size):
+            if embed_type == "passage":
+                vectors_batches = embedding_model.passage_embed(batch_docs, batch_size=batch_size)
+            elif embed_type == "query":
+                vectors_batches = (
+                    list(embedding_model.query_embed(query=query))[0] for query in batch_docs
+                )
+            elif embed_type == "default":
+                vectors_batches = embedding_model.embed(batch_docs, batch_size=batch_size)
+            else:
+                raise ValueError(f"Unknown embed type: {embed_type}")
+            for vector in vectors_batches:
+                yield vector.tolist()
+
+    def _get_vector_field_name(self) -> str:
+        model_name = self.embedding_model_name.split("/")[-1].lower()
+        return f"fast-{model_name}"
+
+    def _scored_points_to_query_responses(
+        self,
+        scored_points: List[types.ScoredPoint],
+    ) -> List[QueryResponse]:
+        response = []
+        for scored_point in scored_points:
+            embedding = None
+            if scored_point.vector is not None:
+                embedding = scored_point.vector.get(self._get_vector_field_name(), None)
+
+            response.append(
+                QueryResponse(
+                    id=scored_point.id,
+                    embedding=embedding,
+                    metadata=scored_point.payload,
+                    document=scored_point.payload.get("document", ""),
+                    score=scored_point.score,
+                )
+            )
+        return response
+
+    async def add(
+        self,
+        collection_name: str,
+        documents: List[str],
+        metadata: Optional[List[Dict[str, Any]]] = None,
+        ids: Optional[List[models.ExtendedPointId]] = None,
+        batch_size: int = 32,
+        **kwargs: Any,
+    ) -> List[str]:
+        """
+        Adds text documents into qdrant collection.
+        If collection does not exist, it will be created with default parameters.
+        Metadata in combination with documents will be added as payload.
+        Documents will be embedded using the specified embedding model.
+
+        If you want to use your own vectors, use `upsert` method instead.
+
+        Args:
+            collection_name (str):
+                Name of the collection to add documents to.
+            documents (List[str]):
+                List of documents to embed and add to the collection.
+            metadata (List[Dict[str, Any]], optional):
+                List of metadata dicts. Defaults to None.
+            ids (List[models.ExtendedPointId], optional):
+                List of ids to assign to documents.
+                If not specified, UUIDs will be generated. Defaults to None.
+            batch_size (int, optional):
+                How many documents to embed and upload in single request. Defaults to 32.
+
+        Raises:
+            ImportError: If fastembed is not installed.
+
+        Returns:
+            List[str]: List of UUIDs of added documents. UUIDs are randomly generated on client side.
+
+        """
+
+        # check if we have fastembed installed
+        embeddings = self._embed_documents(
+            documents=documents,
+            embedding_model_name=self.embedding_model_name,
+            batch_size=batch_size,
+            embed_type="passage",
+        )
+
+        if metadata is None:
+            metadata = [{} for _ in range(len(documents))]
+        else:
+            assert len(metadata) == len(
+                documents
+            ), f"metadata length mismatch: {len(metadata)} != {len(documents)}"
+
+        payloads = ({"document": doc, **metadata} for doc, metadata in zip(documents, metadata))
+
+        if ids is None:
+            ids = [uuid.uuid4().hex for _ in range(len(documents))]
+
+        embeddings_size, distance = self._get_model_params(model_name=self.embedding_model_name)
+
+        vector_field_name = self._get_vector_field_name()
+
+        # Check if collection by same name exists, if not, create it
+        try:
+            collection_info = await self.get_collection(collection_name=collection_name)
+        except Exception:
+            await self.create_collection(
+                collection_name=collection_name,
+                vectors_config={
+                    vector_field_name: models.VectorParams(size=embeddings_size, distance=distance)
+                },
+            )
+            collection_info = await self.get_collection(collection_name=collection_name)
+
+        # Check if collection has compatible vector params
+        assert isinstance(
+            collection_info.config.params.vectors, dict
+        ), f"Collection have incompatible vector params: {collection_info.config.params.vectors}"
+
+        assert (
+            vector_field_name in collection_info.config.params.vectors
+        ), f"Collection have incompatible vector params: {collection_info.config.params.vectors}, expected {vector_field_name}"
+
+        vector_params = collection_info.config.params.vectors[vector_field_name]
+
+        assert (
+            embeddings_size == vector_params.size
+        ), f"Embedding size mismatch: {embeddings_size} != {vector_params.size}"
+
+        assert (
+            distance == vector_params.distance
+        ), f"Distance mismatch: {distance} != {vector_params.distance}"
+
+        records = (
+            models.Record(id=idx, payload=payload, vector={vector_field_name: vector})
+            for idx, payload, vector in zip(ids, payloads, embeddings)
+        )
+
+        await self.upload_records(
+            collection_name=collection_name,
+            records=records,
+            wait=True,
+            **kwargs,
+        )
+
+        return ids
+
+    async def query(
+        self,
+        collection_name: str,
+        query_text: str,
+        query_filter: Optional[models.Filter] = None,
+        limit: int = 10,
+        **kwargs: Any,
+    ) -> List[QueryResponse]:
+        """
+        Search for documents in a collection.
+        This method automatically embeds the query text using the specified embedding model.
+        If you want to use your own query vector, use `search` method instead.
+
+        Args:
+            collection_name: Collection to search in
+            query_text:
+                Text to search for. This text will be embedded using the specified embedding model.
+                And then used as a query vector.
+            query_filter:
+                - Exclude vectors which doesn't fit given conditions.
+                - If `None` - search among all vectors
+            limit: How many results return
+            **kwargs: Additional search parameters. See `qdrant_client.models.SearchRequest` for details.
+
+        Returns:
+            List[types.ScoredPoint]: List of scored points.
+
+        """
+        embedding_model_inst = self._get_or_init_model(model_name=self.embedding_model_name)
+        embeddings = list(embedding_model_inst.query_embed(query=query_text))
+        query_vector = embeddings[0]
+
+        return self._scored_points_to_query_responses(
+            await self.search(
+                collection_name=collection_name,
+                query_vector=models.NamedVector(
+                    name=self._get_vector_field_name(), vector=query_vector.tolist()
+                ),
+                query_filter=query_filter,
+                limit=limit,
+                with_payload=True,
+                **kwargs,
+            )
+        )
+
+    async def query_batch(
+        self,
+        collection_name: str,
+        query_texts: List[str],
+        query_filter: Optional[models.Filter] = None,
+        limit: int = 10,
+        **kwargs: Any,
+    ) -> List[List[QueryResponse]]:
+        """
+        Search for documents in a collection with batched query.
+        This method automatically embeds the query text using the specified embedding model.
+
+        Args:
+            collection_name: Collection to search in
+            query_texts:
+                A list of texts to search for. Each text will be embedded using the specified embedding model.
+                And then used as a query vector for a separate search requests.
+            query_filter:
+                - Exclude vectors which doesn't fit given conditions.
+                - If `None` - search among all vectors
+                This filter will be applied to all search requests.
+            limit: How many results return
+            **kwargs: Additional search parameters. See `qdrant_client.models.SearchRequest` for details.
+
+        Returns:
+            List[List[QueryResponse]]: List of lists of responses for each query text.
+
+        """
+        embedding_model_inst = self._get_or_init_model(model_name=self.embedding_model_name)
+        query_vectors = [
+            list(embedding_model_inst.query_embed(query=query_text))[0]
+            for query_text in query_texts
+        ]
+        requests = []
+        for vector in query_vectors:
+            request = models.SearchRequest(
+                vector=models.NamedVector(
+                    name=self._get_vector_field_name(), vector=vector.tolist()
+                ),
+                filter=query_filter,
+                limit=limit,
+                with_payload=True,
+                **kwargs,
+            )
+
+            requests.append(request)
+
+        return [
+            self._scored_points_to_query_responses(response)
+            for response in await self.search_batch(
+                collection_name=collection_name,
+                requests=requests,
+            )
+        ]
---
+++ tests/check_async_client.py
@@ -0,0 +1,234 @@
+import asyncio
+import random
+
+import grpc.aio._call
+import numpy as np
+
+import qdrant_client.http.exceptions
+from qdrant_client import models
+from qdrant_client.async_qdrant_client import AsyncQdrantClient
+
+COLLECTION_NAME = "async_test_collection"
+
+
+async def main():
+    client = AsyncQdrantClient(prefer_grpc=True)
+    collection_params = dict(
+        collection_name=COLLECTION_NAME,
+        vectors_config=models.VectorParams(size=10, distance=models.Distance.EUCLID),
+    )
+    try:
+        await client.create_collection(**collection_params)
+    except (
+        qdrant_client.http.exceptions.UnexpectedResponse,
+        grpc.aio._call.AioRpcError,
+    ):
+        print("collection already exists")
+        await client.delete_collection(COLLECTION_NAME)
+        await client.create_collection(**collection_params)
+    await client.recreate_collection(**collection_params)
+
+    print(await client.get_collection(COLLECTION_NAME))
+    print(await client.get_collections())
+
+    await client.update_collection(
+        COLLECTION_NAME, hnsw_config=models.HnswConfigDiff(m=32, ef_construct=120)
+    )
+
+    alias_name = COLLECTION_NAME + "_alias"
+    await client.update_collection_aliases(
+        change_aliases_operations=[
+            models.CreateAliasOperation(
+                create_alias=models.CreateAlias(
+                    collection_name=COLLECTION_NAME, alias_name=alias_name
+                )
+            )
+        ]
+    )
+    print(await client.get_aliases())
+    print(await client.get_collection_aliases(COLLECTION_NAME))
+    await client.update_collection_aliases(
+        change_aliases_operations=[
+            models.DeleteAliasOperation(delete_alias=models.DeleteAlias(alias_name=alias_name))
+        ]
+    )
+    assert (await client.get_aliases()).aliases == []
+
+    await client.upsert(
+        collection_name=COLLECTION_NAME,
+        points=[
+            models.PointStruct(
+                id=i,
+                vector=np.random.rand(10).tolist(),
+                payload={"random_dig": random.randint(1, 100)},
+            )
+            for i in range(100)
+        ],
+    )
+    assert (await client.count(COLLECTION_NAME)).count == 100
+
+    assert len((await client.scroll(COLLECTION_NAME, limit=2))[0]) == 2
+
+    assert (
+        len(
+            await client.search(
+                COLLECTION_NAME,
+                query_vector=np.random.rand(10).tolist(),  # type: ignore
+                limit=10,
+            )
+        )
+        == 10
+    )
+
+    assert (
+        len(
+            await client.search_batch(
+                COLLECTION_NAME,
+                requests=[
+                    models.SearchRequest(vector=np.random.rand(10).tolist(), limit=10)
+                    for _ in range(3)
+                ],
+            )
+        )
+        == 3
+    )
+
+    assert (
+        len(
+            (
+                await client.search_groups(
+                    COLLECTION_NAME,
+                    query_vector=np.random.rand(10).tolist(),  # type: ignore
+                    limit=4,
+                    group_by="random_dig",
+                )
+            ).groups
+        )
+        == 4
+    )
+
+    assert len(await client.recommend(COLLECTION_NAME, positive=[0], limit=5)) == 5
+    assert (
+        len(
+            (
+                await client.recommend_groups(
+                    COLLECTION_NAME, positive=[1], group_by="random_dig", limit=6
+                )
+            ).groups
+        )
+        == 6
+    )
+    assert (
+        len(
+            (
+                await client.recommend_batch(
+                    COLLECTION_NAME,
+                    requests=[models.RecommendRequest(positive=[2], limit=7)],
+                )
+            )[0]
+        )
+        == 7
+    )
+
+    assert len(await client.retrieve(COLLECTION_NAME, ids=[3, 5])) == 2
+
+    await client.create_payload_index(
+        COLLECTION_NAME,
+        field_name="random_dig",
+        field_schema=models.PayloadSchemaType.INTEGER,
+    )
+    assert "random_dig" in (await client.get_collection(COLLECTION_NAME)).payload_schema
+
+    await client.delete_payload_index(COLLECTION_NAME, field_name="random_dig")
+    assert "random_dig" not in (await client.get_collection(COLLECTION_NAME)).payload_schema
+
+    assert not (await client.lock_storage(reason="test")).write
+    assert (await client.get_locks()).write
+    assert (await client.unlock_storage()).write
+    assert not (await client.get_locks()).write
+
+    assert isinstance(await client.create_snapshot(COLLECTION_NAME), models.SnapshotDescription)
+    snapshots = await client.list_snapshots(COLLECTION_NAME)
+    assert len(snapshots) == 1
+    # await client.upsert(COLLECTION_NAME, points=[models.PointStruct(id=101, vector=np.random.rand(10).tolist())])
+    # assert (await client.get_collection(COLLECTION_NAME)).vectors_count == 101
+    # await client.recover_snapshot(collection_name=COLLECTION_NAME, location=...)
+    # assert (await client.get_collection(COLLECTION_NAME)).vectors_count == 100
+    await client.delete_snapshot(COLLECTION_NAME, snapshot_name=snapshots[0].name)
+    assert len(await client.list_snapshots(COLLECTION_NAME)) == 0
+
+    assert isinstance(await client.create_full_snapshot(), models.SnapshotDescription)
+    snapshots = await client.list_full_snapshots()
+    assert len(snapshots) == 1
+
+    await client.delete_full_snapshot(snapshot_name=snapshots[0].name)
+    assert len(await client.list_full_snapshots()) == 0
+
+    assert isinstance(
+        await client.create_shard_snapshot(COLLECTION_NAME, shard_id=0),
+        models.SnapshotDescription,
+    )
+    snapshots = await client.list_shard_snapshots(COLLECTION_NAME, shard_id=0)
+    assert len(snapshots) == 1
+    # await client.upsert(COLLECTION_NAME, points=[models.PointStruct(id=101, vector=np.random.rand(10).tolist())])
+    # assert (await client.get_collection(COLLECTION_NAME)).vectors_count == 101
+    # await client.recover_shard_snapshot(collection_name=COLLECTION_NAME, location=..., shard_id=0)
+    # assert (await client.get_collection(COLLECTION_NAME)).vectors_count == 100
+    await client.delete_shard_snapshot(
+        COLLECTION_NAME, snapshot_name=snapshots[0].name, shard_id=0
+    )
+    assert len(await client.list_shard_snapshots(COLLECTION_NAME, shard_id=0)) == 0
+
+    await client.delete_vectors(COLLECTION_NAME, vectors=[""], points=[0])
+    assert (await client.retrieve(COLLECTION_NAME, ids=[0]))[0].vector is None
+
+    await client.update_vectors(
+        COLLECTION_NAME,
+        points=[models.PointVectors(id=0, vector=[1.0] * 10)],
+    )
+    assert (await client.retrieve(COLLECTION_NAME, ids=[0], with_vectors=True))[0].vector == [
+        1.0
+    ] * 10
+
+    await client.delete(COLLECTION_NAME, points_selector=[0])
+    assert (await client.count(COLLECTION_NAME)).count == 99
+
+    await client.batch_update_points(
+        COLLECTION_NAME,
+        update_operations=[
+            models.UpsertOperation(
+                upsert=models.PointsList(points=[models.PointStruct(id=0, vector=[1.0] * 10)])
+            )
+        ],
+    )
+    assert (await client.count(COLLECTION_NAME)).count == 100
+
+    await client.set_payload(COLLECTION_NAME, payload={"added_payload": "zero"}, points=[0])
+    assert (await client.retrieve(COLLECTION_NAME, ids=[0], with_payload=["added_payload"]))[
+        0
+    ].payload == {"added_payload": "zero"}
+    await client.overwrite_payload(
+        COLLECTION_NAME, payload={"overwritten": True, "rand_digit": 2023}, points=[1]
+    )
+    assert (await client.retrieve(COLLECTION_NAME, ids=[1]))[0].payload == {
+        "overwritten": True,
+        "rand_digit": 2023,
+    }
+
+    await client.delete_payload(COLLECTION_NAME, keys=["added_payload"], points=[0])
+    assert (await client.retrieve(COLLECTION_NAME, ids=[0], with_payload=["added_payload"]))[
+        0
+    ].payload == {}
+    await client.clear_payload(COLLECTION_NAME, points_selector=[1])
+    assert (await client.retrieve(COLLECTION_NAME, ids=[1]))[0].payload == {}
+
+    # region teardown
+    await client.delete_collection(COLLECTION_NAME)
+    collections = await client.get_collections()
+    assert any(collection.name != COLLECTION_NAME for collection in collections.collections)
+    await client.close()
+    # endregion
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
