012aad45f8139b67248ade39c2d05af5e7b4431e
NirantK <nirant.bits@gmail.com>
2023-10-26 17:36:35 +0530

    * chore(generate_docs.sh): comment out the installation of pandoc * chore(generate_docs_netlify.sh): comment out the installation of pandoc

--- tools/generate_docs.sh
+++ tools/generate_docs.sh
@@ -6,6 +6,6 @@ set -e
 cd "$(dirname "$0")/../"
 
 
-sudo apt-get install -y pandoc
+# sudo apt-get install -y pandoc
 poetry run sphinx-apidoc --force --separate --no-toc -o docs/source qdrant_client
 poetry run sphinx-build docs/source docs/html
--- tools/generate_docs_netlify.sh
+++ tools/generate_docs_netlify.sh
@@ -9,7 +9,7 @@ pip install qdrant-client
 
 pip install sphinx==4.5.0
 pip install "git+https://github.com/qdrant/qdrant_sphinx_theme.git@master#egg=qdrant-sphinx-theme"
-sudo apt-get install -y pandoc
+# sudo apt-get install -y pandoc
 
 
 sphinx-build docs/source docs/html
