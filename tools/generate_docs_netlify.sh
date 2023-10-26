d60e4a346437cedebaa49486cdedf0a2476e6c4b
NirantK <nirant.bits@gmail.com>
2023-10-26 17:40:39 +0530

    * chore(generate_docs_netlify.sh): add nbsphinx package installation * chore(generate_docs_netlify.sh): install pandoc using apt-get

--- tools/generate_docs_netlify.sh
+++ tools/generate_docs_netlify.sh
@@ -9,7 +9,8 @@ pip install qdrant-client
 
 pip install sphinx==4.5.0
 pip install "git+https://github.com/qdrant/qdrant_sphinx_theme.git@master#egg=qdrant-sphinx-theme"
-# sudo apt-get install -y pandoc
+pip install nbsphinx==0.9.3
+sudo apt-get install -y pandoc
 
 
 sphinx-build docs/source docs/html
