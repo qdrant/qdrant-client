7a119b7f91141ae04f5c06f93a912ae8213c79eb
NirantK <nirant.bits@gmail.com>
2023-10-26 19:35:54 +0530

    * fix(generate_docs.sh): install pandoc using apt-get before running sphinx-apidoc

--- tools/generate_docs.sh
+++ tools/generate_docs.sh
@@ -5,6 +5,5 @@ set -e
 # Ensure current path is project root
 cd "$(dirname "$0")/../"
 
-# sudo apt-get install -y pandoc
 poetry run sphinx-apidoc --force --separate --no-toc -o docs/source qdrant_client
 poetry run sphinx-build docs/source docs/html
