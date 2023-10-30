e0352c88c231dcc52fffac8625a4fb551d8e34cf
NirantK <nirant.bits@gmail.com>
2023-10-31 00:39:46 +0530

    * chore(generate_docs_netlify.sh): add ipython package to the dependencies

--- tools/generate_docs_netlify.sh
+++ tools/generate_docs_netlify.sh
@@ -10,6 +10,7 @@ brew install pandoc
 pip install qdrant-client
 
 pip install sphinx==4.5.0
+pip install ipython
 pip install "git+https://github.com/qdrant/qdrant_sphinx_theme.git@master#egg=qdrant-sphinx-theme"
 pip install nbsphinx==0.9.3
 pip install --upgrade Pygments==2.16.1
