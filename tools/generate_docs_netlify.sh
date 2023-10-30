2ad3bb51413f0e3ee4693bd8f50d84e75cc55cd5
NirantK <nirant.bits@gmail.com>
2023-10-31 00:29:13 +0530

    * chore(generate_docs_netlify.sh): add Pygments package as a dependency for generating docs

--- tools/generate_docs_netlify.sh
+++ tools/generate_docs_netlify.sh
@@ -12,5 +12,5 @@ pip install qdrant-client
 pip install sphinx==4.5.0
 pip install "git+https://github.com/qdrant/qdrant_sphinx_theme.git@master#egg=qdrant-sphinx-theme"
 pip install nbsphinx==0.9.3
-
+pip install --upgrade Pygments==2.16.1
 sphinx-build docs/source docs/html
