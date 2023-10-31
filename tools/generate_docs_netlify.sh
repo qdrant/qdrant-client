ef49b6f1e6e8fbd24a623b4cad4cef986847fdac
Nirant <NirantK@users.noreply.github.com>
2023-10-31 19:24:14 +0530

    Rewrite Docstrings + Change Netlify build to branch from Pip (#359)
    * * chore(qdrant_client.py): update ordering parameter description
    
    * * fix(qdrant_client.py): fix docstring formatting and add missing type hints in the update_point method
    
    * * chore(async_qdrant_client.py): update docstring for `upsert` method
    
    * * chore(qdrant_client.py): reformat and clarify ordering parameter description
    
    * * chore(async_qdrant_client.py): update method signatures and add missing docstring for `insert` and `update` methods
    * chore(qdrant_client.py): update method signatures and add missing docstring for `insert` and `update` methods
    
    * *chore(qdrant_client.py): fix indentation errors
    
    * *chore(async_qdrant_client.py): fix indentation errors
    
    * * chore(.gitignore): add ignore rules for local_cache and db directories in docs/source/examples
    
    * * chore(generate_docs_netlify.sh): add Pygments package as a dependency for generating docs
    
    * * chore(conf.py): add IPython.sphinxext.ipython_console_highlighting to the list of extensions
    
    * * chore(generate_docs_netlify.sh): add ipython package to the dependencies
    
    * * chore(generate_docs_netlify.sh): update pip install command to install project in editable mode
    
    * * chore(generate_docs_netlify.sh): update script to use poetry for dependency management
    
    * * chore(generate_docs_netlify.sh): add export PATH command to include /opt/buildhome/.local/bin in the PATH
    
    * * chore(async_qdrant_client.py, qdrant_client.py): update documentation for scroll
    
    * * chore(async_qdrant_client.py): update consistency parameter docs
    * chore(qdrant_client.py): update consistency parameter docs
    
    * * chore(qdrant_client): fix priority indentation in docstrings
    
    * * docs(index.rst): update reference link for DataTypes aka Models to Models

--- tools/generate_docs_netlify.sh
+++ tools/generate_docs_netlify.sh
@@ -7,10 +7,13 @@ cd "$(dirname "$0")/../"
 
 brew install pandoc
 
-pip install qdrant-client
+curl -sSL https://install.python-poetry.org | python3 -
+export PATH="/opt/buildhome/.local/bin:$PATH"
+poetry install
 
 pip install sphinx==4.5.0
+pip install ipython
 pip install "git+https://github.com/qdrant/qdrant_sphinx_theme.git@master#egg=qdrant-sphinx-theme"
 pip install nbsphinx==0.9.3
-
+pip install --upgrade Pygments==2.16.1
 sphinx-build docs/source docs/html
