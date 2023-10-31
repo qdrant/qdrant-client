eecc378444a85dbfffa15f7b6ab8fc5562fc4809
NirantK <nirant.bits@gmail.com>
2023-10-31 19:27:40 +0530

    * chore(generate_docs_netlify.sh): remove unnecessary pip installations and fix newline at end of file

--- tools/generate_docs_netlify.sh
+++ tools/generate_docs_netlify.sh
@@ -11,9 +11,4 @@ curl -sSL https://install.python-poetry.org | python3 -
 export PATH="/opt/buildhome/.local/bin:$PATH"
 poetry install
 
-pip install sphinx==4.5.0
-pip install ipython
-pip install "git+https://github.com/qdrant/qdrant_sphinx_theme.git@master#egg=qdrant-sphinx-theme"
-pip install nbsphinx==0.9.3
-pip install --upgrade Pygments==2.16.1
 sphinx-build docs/source docs/html
