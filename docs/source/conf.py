dc6361daeb717a63a57b54574338bd40a4eac53f
NirantK <nirant.bits@gmail.com>
2023-11-01 10:41:03 +0530

    * chore(conf.py): exclude local directory from documentation generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,7 +71,7 @@ templates_path = ["_templates"]
 exclude_patterns = [
     "*qdrant_openapi_client*",
     "*grpc*",
-    "*tests*" # tests are not part of the documentation
+    "*local*" # tests are not part of the documentation
 ]
 # -- Options for HTML output -------------------------------------------------
 
