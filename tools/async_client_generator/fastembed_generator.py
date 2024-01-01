1040632dc59ff7c3cc0ff65b20f7f5c9267c01ea
Praveen Palanisamy <4770482+praveen-palanisamy@users.noreply.github.com>
2023-12-31 23:15:50 -0800

    Update async client gen to include set_model_params

--- tools/async_client_generator/fastembed_generator.py
+++ tools/async_client_generator/fastembed_generator.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     generator = FastembedGenerator(
         keep_sync=[
             "__init__",
-            "set_model",
+            "set_model_params",
             "get_vector_field_name",
             "get_fastembed_vector_params",
         ],
