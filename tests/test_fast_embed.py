70754dbd919adc4a6db7ed264f47f4d7b100cb84
Praveen Palanisamy <4770482+praveen-palanisamy@users.noreply.github.com>
2024-01-07 09:48:58 -0800

    Allow fastembed embedding model params configuration in parity with fastembed APIs (#416)
    * Update set_model -> set_model_params;
    Allow setting all fastembed.DefaultEmbedding init args for full pass-through
    
    * Add fastembed test for setting custom embedding model params
    
    * Update async client gen to include set_model_params
    
    * Update Async client APIs
    
    * Add explicit Optional since PEP 484 is still in effect
    
    * Regenerate async client APIs
    
    * Revert name change: set_model_params -> set_model
    
    * Add **kwargs to set_model method sig
    
    * Update name set_model_params -> set_model
    
    * Regenerate async client APIs
    
    * Use and pass **kwargs
    
    * Regenerate async client APIs using Py 3.10.x

--- tests/test_fast_embed.py
+++ tests/test_fast_embed.py
@@ -86,6 +86,49 @@ def test_query(
     assert len(search_result) > 0
 
 
+def test_set_model(
+    local_client: QdrantClient = QdrantClient(":memory:"),
+    collection_name: str = "demo_collection",
+    docs: List[str] = None,
+):
+    import tempfile
+
+    if docs is None:
+        docs = [
+            "Qdrant has native Fastembed integration",
+            "You can just add documents and query them to do semantic search",
+        ]
+
+    if not local_client._is_fastembed_installed:
+        pytest.skip("FastEmbed is not installed, skipping test")
+
+    embedding_model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    max_length = 384
+    temp_cache_dir = tempfile.TemporaryDirectory()
+    local_client.set_model(
+        embedding_model_name=embedding_model_name,
+        max_length=max_length,
+        cache_dir=temp_cache_dir.name,
+    )
+    # Check if the model is initialized & cls.embeddings_models is set with expected values
+    dim, dist = local_client._get_model_params(embedding_model_name)
+    assert dim == max_length
+
+    # Assert that the cache directory has a directory which contains `model.onnx` file
+    import os
+
+    assert os.path.exists(
+        os.path.join(
+            temp_cache_dir.name,
+            f"fast-{embedding_model_name.split('/')[1]}",
+            "model.onnx",
+        )
+    )
+    # Use the initialized model to add documents with vector embeddings
+    local_client.add(collection_name=collection_name, documents=docs)
+    assert local_client.count(collection_name).count == 2
+
+
 if __name__ == "__main__":
     test_add_without_query()
     test_query()
