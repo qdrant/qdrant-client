c3a7adf4438b5547bcf32e40ecb21bb193d60321
George Panchuk <george.panchuk@qdrant.tech>
2024-03-12 22:44:14 +0100

    fix: handle tzinfo in HH format in local mode

--- qdrant_client/local/datetime_utils.py
+++ qdrant_client/local/datetime_utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 from typing import Optional
 
 # These are the formats accepted by qdrant core
@@ -26,14 +26,31 @@ def parse(date_str: str) -> Optional[datetime]:
         Optional[datetime]: the datetime if the string is valid, otherwise None
     """
 
-    for fmt in available_formats:
-        try:
-            dt = datetime.strptime(date_str, fmt)
-            if dt.tzinfo is None:
-                # Assume UTC if no timezone is provided
-                dt = dt.replace(tzinfo=timezone.utc)
-            return dt
-        except ValueError:
-            pass
+    def parse_available_formats(date_str: str) -> Optional[datetime]:
+        for fmt in available_formats:
+            try:
+                dt = datetime.strptime(date_str, fmt)
+                if dt.tzinfo is None:
+                    # Assume UTC if no timezone is provided
+                    dt = dt.replace(tzinfo=timezone.utc)
+                return dt
+            except ValueError:
+                pass
 
-    return None
+    parsed_dt = parse_available_formats(date_str)
+    if parsed_dt is not None:
+        return parsed_dt
+
+    if "+" in date_str:
+        dt_str, tz_str = date_str.split("+")
+        tz_offset = int(tz_str.lower().replace("z", ""))
+    elif "-" in date_str[-4:]:
+        dt_str, tz_str = date_str.rsplit("-", 1)
+        tz_offset = -int(tz_str.lower().replace("z", ""))
+    else:
+        return None
+
+    parsed_dt = parse_available_formats(dt_str)
+    if parsed_dt is not None:
+        parsed_dt = parsed_dt.replace(tzinfo=timezone(timedelta(hours=tz_offset)))
+    return parsed_dt
