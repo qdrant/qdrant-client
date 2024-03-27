822aefa1c1c55097fdd65dd4af26cc718a37d91c
Andrey Vasnetsov <andrey@vasnetsov.com>
2024-03-27 13:57:58 +0100

    WIP: hybrid search with fastembed (#553)
    * WIP: hybrid search with fastembed
    
    * hybrid queries with fastembed
    
    * test for hybrid
    
    * fix typo
    
    * new: extend hybrid search tests, fix mypy, small refactoring (#554)
    
    * refactor: align model name parameters in setters, update tests
    
    * fix: fix async
    
    * fix: add a good test, fix sparse vectors in query batch
    
    * refactoring: reduce branching, refactor fastembed tests
    
    ---------
    
    Co-authored-by: George <george.panchuk@qdrant.tech>

--- tests/test_fastembed.py
+++ tests/test_fastembed.py
@@ -1,122 +1,128 @@
-from typing import Any, Dict, List, Union
-
 import pytest
 
 from qdrant_client import QdrantClient
 
+DOCS_EXAMPLE = {
+    "documents": [
+        "Qdrant has Langchain integrations",
+        "Qdrant also has Llama Index integrations",
+    ],
+    "metadata": [{"source": "Langchain-docs"}, {"source": "LlamaIndex-docs"}],
+    "ids": [42, 2000],
+}
 
-def test_add_without_query(
-    local_client: QdrantClient = QdrantClient(":memory:"),
-    collection_name: str = "demo_collection",
-    docs: List[str] = None,
-):
-    if docs is None:
-        docs = [
-            "Qdrant has Langchain integrations",
-            "Qdrant also has Llama Index integrations",
-        ]
+
+def test_dense():
+    local_client = QdrantClient(":memory:")
+    collection_name = "demo_collection"
+    docs = [
+        "Qdrant has Langchain integrations",
+        "Qdrant also has Llama Index integrations",
+    ]
 
     if not local_client._FASTEMBED_INSTALLED:
-        pytest.skip("FastEmbed is not installed, skipping test")
-
-    local_client.add(collection_name=collection_name, documents=docs)
-    assert local_client.count(collection_name).count == 2
-
-
-def test_no_install(
-    local_client: QdrantClient = None,
-    collection_name: str = "demo_collection",
-    docs: Dict[str, List[Union[str, int, Any]]] = None,
-):
-    if local_client is None:
-        local_client = QdrantClient(":memory:")
-
-    if docs is None:
-        docs = {
-            "documents": [
-                "Qdrant has Langchain integrations",
-                "Qdrant also has Llama Index integrations",
-            ],
-            "metadatas": [{"source": "Langchain-docs"}, {"source": "LlamaIndex-docs"}],
-            "ids": [42, 2],
-        }
-
-    # When FastEmbed is not installed, the add method should raise an ImportError
-    if local_client._FASTEMBED_INSTALLED:
-        pytest.skip("FastEmbed is installed, skipping test")
-    else:
         with pytest.raises(ImportError):
             local_client.add(collection_name, docs)
+    else:
+        local_client.add(collection_name=collection_name, documents=docs)
+        assert local_client.count(collection_name).count == 2
 
+        local_client.add(collection_name=collection_name, **DOCS_EXAMPLE)
+        assert local_client.count(collection_name).count == 4
 
-def test_query(
-    local_client: QdrantClient = None,
-    collection_name: str = "demo_collection",
-    docs: Dict[str, List[Union[str, int, Any]]] = None,
-):
-    if local_client is None:
-        local_client = QdrantClient(":memory:")
-
-    if docs is None:
-        docs = {
-            "documents": [
-                "Qdrant has Langchain integrations",
-                "Qdrant also has Llama Index integrations",
-            ],
-            "metadatas": [{"source": "Langchain-docs"}, {"source": "LlamaIndex-docs"}],
-            "ids": [42, 2],
+        id_ = DOCS_EXAMPLE["ids"][0]
+        record = local_client.retrieve(collection_name, ids=[id_])[0]
+        assert record.payload == {
+            "document": DOCS_EXAMPLE["documents"][0],
+            **DOCS_EXAMPLE["metadata"][0],
         }
+
+        search_result = local_client.query(
+            collection_name=collection_name, query_text="This is a query document"
+        )
+
+        assert len(search_result) > 0
+
+
+def test_hybrid_query():
+    local_client = QdrantClient(":memory:")
+    collection_name = "hybrid_collection"
 
     if not local_client._FASTEMBED_INSTALLED:
         pytest.skip("FastEmbed is not installed, skipping test")
 
-    local_client.add(
-        collection_name=collection_name,
-        documents=docs["documents"],
-        metadata=docs["metadatas"],
-        ids=docs["ids"],
-    )
-    assert local_client.count(collection_name).count == 2
-    # Query the added documents
+    local_client.set_sparse_model(embedding_model_name="prithvida/Splade_PP_en_v1")
 
-    search_result = local_client.query(
+    local_client.add(collection_name=collection_name, **DOCS_EXAMPLE)
+
+    hybrid_search_result = local_client.query(
         collection_name=collection_name, query_text="This is a query document"
     )
 
-    assert len(search_result) > 0
+    assert len(hybrid_search_result) > 0
+
+    local_client.set_sparse_model(None)
+    dense_search_result = local_client.query(
+        collection_name=collection_name, query_text="This is a query document"
+    )
+    assert len(dense_search_result) > 0
+
+    assert (
+        hybrid_search_result[0].score != dense_search_result[0].score
+    )  # hybrid search has score from fusion
 
 
-def test_set_model(
-    local_client: QdrantClient = QdrantClient(":memory:"),
-    collection_name: str = "demo_collection",
-    docs: List[str] = None,
-):
-    import tempfile
+def test_query_batch():
+    local_client = QdrantClient(":memory:")
 
-    if docs is None:
-        docs = [
-            "Qdrant has native Fastembed integration",
-            "You can just add documents and query them to do semantic search",
-        ]
+    dense_collection_name = "dense_collection"
+    hybrid_collection_name = "hybrid_collection"
 
     if not local_client._FASTEMBED_INSTALLED:
         pytest.skip("FastEmbed is not installed, skipping test")
 
+    local_client.add(collection_name=dense_collection_name, **DOCS_EXAMPLE)
+    query_texts = ["This is a query document", "This is another query document"]
+    dense_search_result = local_client.query_batch(
+        collection_name=dense_collection_name, query_texts=query_texts
+    )
+    assert len(dense_search_result) == len(query_texts)
+    assert all(len(result) > 0 for result in dense_search_result)
+
+    local_client.set_sparse_model(embedding_model_name="prithvida/Splade_PP_en_v1")
+
+    local_client.add(collection_name=hybrid_collection_name, **DOCS_EXAMPLE)
+
+    hybrid_search_result = local_client.query_batch(
+        collection_name=hybrid_collection_name, query_texts=query_texts
+    )
+
+    assert len(hybrid_search_result) == len(query_texts)
+    assert all(len(result) > 0 for result in hybrid_search_result)
+
+    single_dense_response = next(iter(dense_search_result))
+    single_hybrid_response = next(iter(hybrid_search_result))
+
+    assert (
+        single_hybrid_response[0].score != single_dense_response[0].score
+    )  # hybrid search has score from fusion
+
+
+def test_set_model():
+    local_client = QdrantClient(":memory:")
+    collection_name = "demo_collection"
     embedding_model_name = "sentence-transformers/all-MiniLM-L6-v2"
-    max_length = 384
+    if not local_client._FASTEMBED_INSTALLED:
+        pytest.skip("FastEmbed is not installed, skipping test")
+
     local_client.set_model(
         embedding_model_name=embedding_model_name,
-        max_length=max_length,
     )
+
     # Check if the model is initialized & cls.embeddings_models is set with expected values
     dim, dist = local_client._get_model_params(embedding_model_name)
-    assert dim == max_length
+    assert dim == 384
 
     # Use the initialized model to add documents with vector embeddings
-    local_client.add(collection_name=collection_name, documents=docs)
+    local_client.add(collection_name=collection_name, **DOCS_EXAMPLE)
     assert local_client.count(collection_name).count == 2
-
-
-if __name__ == "__main__":
-    test_add_without_query()
-    test_query()
