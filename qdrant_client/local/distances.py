3fcc896b4c24ca3eb7f8a8ea55cc325d00dde71c
George Panchuk <george.panchuk@qdrant.tech>
2024-04-02 11:00:44 +0200

    fix: update query vector type, fix invalid vector type in search in local mode

--- qdrant_client/local/distances.py
+++ qdrant_client/local/distances.py
@@ -40,7 +40,7 @@ class ContextQuery:
         self.context_pairs = context_pairs
 
 
-QueryVector = Union[DiscoveryQuery, ContextQuery, RecoQuery, types.NumpyArray, SparseVector]
+QueryVector = Union[DiscoveryQuery, ContextQuery, RecoQuery, SparseVector]
 
 
 class DistanceOrder(str, Enum):
