e25b005531b1fdc6c21e1b1f8d46df77ed208095
George <george.panchuk@qdrant.tech>
2024-04-02 15:58:48 +0200

    fix: fix update sparse vectors local mode (#559)
    * fix: fix update sparse vectors local mode
    
    * fix: validate sparse vector before update
    
    * fix: update update_named_vectors signature

--- tests/congruence_tests/test_optional_vectors.py
+++ tests/congruence_tests/test_optional_vectors.py
@@ -6,11 +6,15 @@ from tests.congruence_tests.test_common import (
     NUM_VECTORS,
     compare_client_results,
     generate_fixtures,
+    generate_sparse_fixtures,
     image_vector_size,
     init_client,
     init_local,
     init_remote,
+    sparse_image_vector_size,
+    sparse_vectors_config,
 )
+from tests.fixtures.points import random_sparse_vectors
 
 
 def test_simple_opt_vectors_search():
@@ -79,6 +83,88 @@ def test_simple_opt_vectors_search():
                 vectors_to_retrieve,
                 with_payload=False,
                 with_vectors=["image", "code"],
+            ),
+            key=lambda x: x.id,
+        ),
+    )
+
+
+def test_simple_opt_sparse_vectors_search():
+    fixture_points = generate_sparse_fixtures()
+
+    local_client = init_local()
+    init_client(
+        local_client,
+        fixture_points,
+        vectors_config={},
+        sparse_vectors_config=sparse_vectors_config,
+    )
+
+    remote_client = init_remote()
+    init_client(
+        remote_client,
+        fixture_points,
+        vectors_config={},
+        sparse_vectors_config=sparse_vectors_config,
+    )
+
+    ids_to_delete = [x for x in range(NUM_VECTORS) if x % 5 == 0]
+
+    vectors_to_retrieve = [x for x in range(20)]
+
+    local_client.delete_vectors(
+        collection_name=COLLECTION_NAME,
+        vectors=["sparse-image"],
+        points=ids_to_delete,
+    )
+    remote_client.delete_vectors(
+        collection_name=COLLECTION_NAME,
+        vectors=["sparse-image"],
+        points=ids_to_delete,
+    )
+
+    compare_client_results(
+        local_client,
+        remote_client,
+        lambda c: sorted(
+            c.retrieve(
+                COLLECTION_NAME,
+                vectors_to_retrieve,
+                with_payload=False,
+                with_vectors=["sparse-image", "sparse-code"],
+            ),
+            key=lambda x: x.id,
+        ),
+    )
+
+    new_vector = random_sparse_vectors({"sparse-image": sparse_image_vector_size})
+    update_vectors = [
+        models.PointVectors(
+            id=i,
+            vector=new_vector,
+        )
+        for i in range(6)
+    ]
+
+    local_client.update_vectors(
+        collection_name=COLLECTION_NAME,
+        points=update_vectors,
+    )
+
+    remote_client.update_vectors(
+        collection_name=COLLECTION_NAME,
+        points=update_vectors,
+    )
+
+    compare_client_results(
+        local_client,
+        remote_client,
+        lambda c: sorted(
+            c.retrieve(
+                COLLECTION_NAME,
+                vectors_to_retrieve,
+                with_payload=False,
+                with_vectors=["sparse-image", "sparse-code"],
             ),
             key=lambda x: x.id,
         ),
