57f44d96a870bec3164de6f284cf8c8bec129d42
Hossam Hagag <90828745+hh-space-invader@users.noreply.github.com>
2024-08-09 22:06:30 +0300

    Add missing timeouts and regenerate grpc (#721)
    * fixed missing grpc timeouts
    
    * added timeouts in the corresponding rest calls of grpc
    
    * regenerated grpc client
    
    * new: add timeout to QdrantClient and AsyncQdrantClient
    
    * fix: remove duplicated timeout arg
    
    ---------
    
    Co-authored-by: George Panchuk <george.panchuk@qdrant.tech>

--- qdrant_client/async_qdrant_client.py
+++ qdrant_client/async_qdrant_client.py
@@ -1159,6 +1159,7 @@ class AsyncQdrantClient(AsyncQdrantFastembedMixin):
         with_vectors: Union[bool, Sequence[str]] = False,
         consistency: Optional[types.ReadConsistency] = None,
         shard_key_selector: Optional[types.ShardKeySelector] = None,
+        timeout: Optional[int] = None,
         **kwargs: Any,
     ) -> Tuple[List[types.Record], Optional[types.PointId]]:
         """Scroll over all (matching) points in the collection.
@@ -1194,6 +1195,9 @@ class AsyncQdrantClient(AsyncQdrantFastembedMixin):
                 This parameter allows to specify which shards should be queried.
                 If `None` - query all shards. Only works for collections with `custom` sharding method.
 
+            timeout:
+                Overrides global timeout for this operation. Unit is seconds.
+
         Returns:
             A pair of (List of points) and (optional offset for the next scroll request).
             If next page offset is `None` - there is no more points in the collection to scroll.
@@ -1209,6 +1213,7 @@ class AsyncQdrantClient(AsyncQdrantFastembedMixin):
             with_vectors=with_vectors,
             consistency=consistency,
             shard_key_selector=shard_key_selector,
+            timeout=timeout,
             **kwargs,
         )
 
@@ -1218,6 +1223,7 @@ class AsyncQdrantClient(AsyncQdrantFastembedMixin):
         count_filter: Optional[types.Filter] = None,
         exact: bool = True,
         shard_key_selector: Optional[types.ShardKeySelector] = None,
+        timeout: Optional[int] = None,
         **kwargs: Any,
     ) -> types.CountResult:
         """Count points in the collection.
@@ -1235,6 +1241,9 @@ class AsyncQdrantClient(AsyncQdrantFastembedMixin):
                 This parameter allows to specify which shards should be queried.
                 If `None` - query all shards. Only works for collections with `custom` sharding method.
 
+            timeout:
+                Overrides global timeout for this operation. Unit is seconds.
+
         Returns:
             Amount of points in the collection matching the filter.
         """
@@ -1244,6 +1253,7 @@ class AsyncQdrantClient(AsyncQdrantFastembedMixin):
             count_filter=count_filter,
             exact=exact,
             shard_key_selector=shard_key_selector,
+            timeout=timeout,
             **kwargs,
         )
 
@@ -1396,6 +1406,7 @@ class AsyncQdrantClient(AsyncQdrantFastembedMixin):
         with_vectors: Union[bool, Sequence[str]] = False,
         consistency: Optional[types.ReadConsistency] = None,
         shard_key_selector: Optional[types.ShardKeySelector] = None,
+        timeout: Optional[int] = None,
         **kwargs: Any,
     ) -> List[types.Record]:
         """Retrieve stored points by IDs
@@ -1426,6 +1437,9 @@ class AsyncQdrantClient(AsyncQdrantFastembedMixin):
                 This parameter allows to specify which shards should be queried.
                 If `None` - query all shards. Only works for collections with `custom` sharding method.
 
+            timeout:
+                Overrides global timeout for this operation. Unit is seconds.
+
         Returns:
             List of points
         """
@@ -1437,6 +1451,7 @@ class AsyncQdrantClient(AsyncQdrantFastembedMixin):
             with_vectors=with_vectors,
             consistency=consistency,
             shard_key_selector=shard_key_selector,
+            timeout=timeout,
             **kwargs,
         )
 
