d67606012e44ef1dc9ccc0784310989560533e72
George Panchuk <george.panchuk@qdrant.tech>
2024-08-12 13:55:39 +0200

    tmp: disable query extension for vector input, etc

--- qdrant_client/http/models/models.py
+++ qdrant_client/http/models/models.py
@@ -590,7 +590,7 @@ class Distance(str, Enum):
     MANHATTAN = "Manhattan"
 
 
-class Document(BaseModel, extra="forbid"):
+class Document(BaseModel):
     text: str = Field(..., description="Text document to be embedded by FastEmbed or Cloud inference server")
     model: Optional[str] = Field(default=None, description="Model name to be used for embedding computation")
 
@@ -2824,7 +2824,6 @@ Vector = Union[
     List[StrictFloat],
     SparseVector,
     List[List[StrictFloat]],
-    Document,
 ]
 VectorStorageType = Union[
     VectorStorageTypeOneOf,
@@ -2848,7 +2847,6 @@ BatchVectorStruct = Union[
     List[List[StrictFloat]],
     List[List[List[StrictFloat]]],
     Dict[StrictStr, List[Vector]],
-    List[Document],
 ]
 PayloadFieldSchema = Union[
     PayloadSchemaType,
@@ -2868,13 +2866,11 @@ VectorInput = Union[
     SparseVector,
     List[List[StrictFloat]],
     ExtendedPointId,
-    Document,
 ]
 VectorStruct = Union[
     List[StrictFloat],
     List[List[StrictFloat]],
     Dict[StrictStr, Vector],
-    Document,
 ]
 WithPayloadInterface = Union[
     StrictBool,
