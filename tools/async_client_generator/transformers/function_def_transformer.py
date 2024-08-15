52edfddff2dd998d4ad231ee7b8fc17fd2606c96
hh-space-invader <h.hagag.ali@gmail.com>
2024-08-15 12:56:39 +0300

    refactor: use kwargs in visit_FunctionDef while handling type_params for Python 3.12+
    - Refactored visit_FunctionDef to utilize **kwargs for argument handling.

--- tools/async_client_generator/transformers/function_def_transformer.py
+++ tools/async_client_generator/transformers/function_def_transformer.py
@@ -14,26 +14,19 @@ class FunctionDefTransformer(ast.NodeTransformer):
         if self._keep_sync(sync_node.name):
             return self.generic_visit(sync_node)
 
-        # Check the Python version
+        async_kwargs = {
+            "name": sync_node.name,
+            "args": sync_node.args,
+            "body": sync_node.body,
+            "decorator_list": sync_node.decorator_list,
+            "returns": sync_node.returns,
+            "type_comment": sync_node.type_comment,
+        }
+
         if sys.version_info >= (3, 12):
-            async_node = ast.AsyncFunctionDef(
-                name=sync_node.name,
-                args=sync_node.args,
-                body=sync_node.body,
-                decorator_list=sync_node.decorator_list,
-                returns=sync_node.returns,
-                type_comment=sync_node.type_comment,
-                type_params=[],
-            )
-        else:
-            async_node = ast.AsyncFunctionDef(
-                name=sync_node.name,
-                args=sync_node.args,
-                body=sync_node.body,
-                decorator_list=sync_node.decorator_list,
-                returns=sync_node.returns,
-                type_comment=sync_node.type_comment,
-            )
+            async_kwargs["type_params"] = []
+
+        async_node = ast.AsyncFunctionDef(**async_kwargs)
 
         async_node.lineno = sync_node.lineno
         async_node.col_offset = sync_node.col_offset
