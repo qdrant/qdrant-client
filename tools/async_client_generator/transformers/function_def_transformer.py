66541becf55d69af5ebbddc58a572718bbd5e2aa
hh-space-invader <h.hagag.ali@gmail.com>
2024-08-15 15:20:15 +0300

    refactor: pass positional arguments for AsyncFunctionDef with conditional type_params handling
    - Refactored visit_FunctionDef to pass parameters as positional arguments to AsyncFunctionDef.

--- tools/async_client_generator/transformers/function_def_transformer.py
+++ tools/async_client_generator/transformers/function_def_transformer.py
@@ -14,19 +14,19 @@ class FunctionDefTransformer(ast.NodeTransformer):
         if self._keep_sync(sync_node.name):
             return self.generic_visit(sync_node)
 
-        async_kwargs = {
-            "name": sync_node.name,
-            "args": sync_node.args,
-            "body": sync_node.body,
-            "decorator_list": sync_node.decorator_list,
-            "returns": sync_node.returns,
-            "type_comment": sync_node.type_comment,
-        }
+        params: List = [
+            sync_node.name,
+            sync_node.args,
+            sync_node.body,
+            sync_node.decorator_list,
+            sync_node.returns,
+            sync_node.type_comment,
+        ]
 
         if sys.version_info >= (3, 12):
-            async_kwargs["type_params"] = []
+            type_params = sync_node.type_params
 
-        async_node = ast.AsyncFunctionDef(**async_kwargs)
+        async_node = ast.AsyncFunctionDef(*params)
 
         async_node.lineno = sync_node.lineno
         async_node.col_offset = sync_node.col_offset
