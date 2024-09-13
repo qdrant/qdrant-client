0a9f1b6db4b25adedfdbe5be4a541504f075dd4e
George Panchuk <george.panchuk@qdrant.tech>
2024-09-13 11:09:49 +0200

    rollback grpcio version

--- tools/generate_grpc_client.sh
+++ tools/generate_grpc_client.sh
@@ -4,7 +4,7 @@ set -e
 
 PROJECT_ROOT="$(pwd)/$(dirname "$0")/../"
 
-pip install "grpcio>=1.48.2,<2.0.0"
+pip install grpcio==1.48.2
 pip install grpcio-tools==1.48.2 # the expected python version to use is 3.10.10
 
 cd $(mktemp -d)
