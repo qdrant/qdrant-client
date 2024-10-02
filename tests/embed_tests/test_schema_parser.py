eb87c55a13e6387d52fcec718b97537f4fa984af
George Panchuk <george.panchuk@qdrant.tech>
2024-10-03 01:23:42 +0200

    new: add local inference inspectors

---
+++ tests/embed_tests/test_schema_parser.py
@@ -0,0 +1,55 @@
+from typing import List
+
+import pytest
+
+from qdrant_client import models
+from qdrant_client.embed.schema_parser import OpenApiSchemaParser
+from qdrant_client.embed.utils import Path
+
+
+def check_path_recursive(plain_path_parts: list[str], paths: List[Path]) -> bool:
+    if not plain_path_parts:
+        return True
+
+    for path in paths:
+        if path.current == plain_path_parts[0]:
+            return check_path_recursive(plain_path_parts[1:], path.tail)
+    return False
+
+
+@pytest.mark.parametrize(
+    "model",
+    [
+        models.Batch,
+        models.ContextPair,
+        models.ContextQuery,
+        models.DiscoverInput,
+        models.DiscoverQuery,
+        models.NearestQuery,
+        models.PointStruct,
+        models.PointVectors,
+        models.PointsBatch,
+        models.PointsList,
+        models.RecommendInput,
+        models.RecommendQuery,
+        models.UpdateVectors,
+        models.UpdateVectorsOperation,
+        models.UpsertOperation,
+        models.Prefetch,
+        models.QueryGroupsRequest,
+        models.QueryRequest,
+        models.QueryRequestBatch,
+        models.UpdateOperations,
+    ],
+)
+def test_parser(model):
+    parser = OpenApiSchemaParser()
+    parser.check_model(model)
+
+    for model_name, plain_paths in parser._cache.items():
+        count = 0
+        paths = parser.path_cache[model_name]
+        for plain_path in plain_paths:
+            count += check_path_recursive(plain_path.split("."), paths)
+
+        assert count == len(plain_paths)
