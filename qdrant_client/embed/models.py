aded0c685811a4f61421bcea0b35a2bd5dbfbb88
George Panchuk <george.panchuk@qdrant.tech>
2024-10-16 23:40:28 +0200

    refactor: update type hint

--- qdrant_client/embed/models.py
+++ qdrant_client/embed/models.py
@@ -1,3 +1,16 @@
+from typing import Union, List
+
+from pydantic import StrictFloat
+
+from qdrant_client.grpc import SparseVector
+from qdrant_client.http.models import ExtendedPointId
 from qdrant_client.models import Document  # type: ignore[attr-defined]
 
-__all__ = ["Document"]
+
+NumericVectorInput = Union[
+    List[StrictFloat],
+    SparseVector,
+    List[List[StrictFloat]],
+    ExtendedPointId,
+]
+__all__ = ["Document", "NumericVectorInput"]
