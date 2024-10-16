1691d43008266e5d4215a008d8406884923ce7cf
George Panchuk <george.panchuk@qdrant.tech>
2024-10-17 00:13:43 +0200

    chore: add docstrings to embed methods

--- qdrant_client/async_qdrant_fastembed.py
+++ qdrant_client/async_qdrant_fastembed.py
@@ -20,7 +20,7 @@ from qdrant_client.async_client_base import AsyncQdrantBase
 from qdrant_client.conversions import common_types as types
 from qdrant_client.conversions.conversion import GrpcToRest
 from qdrant_client.embed.embed_inspector import InspectorEmbed
-from qdrant_client.embed.models import NumericVectorInput
+from qdrant_client.embed.models import NumericVector, NumericVectorStruct
 from qdrant_client.embed.utils import Path
 from qdrant_client.fastembed_common import QueryResponse
 from qdrant_client.http import models
@@ -715,6 +715,17 @@ class AsyncQdrantFastembedMixin(AsyncQdrantBase):
             None,
         ],
     ) -> Optional[models.Query]:
+        """Resolves query interface into a models.Query object
+
+        Args:
+            query: models.QueryInterface - query as a model or a plain structure like List[float]
+
+        Returns:
+            Optional[models.Query]: query as it was, models.Query(nearest=query) or None
+
+        Raises:
+            ValueError: if query is not of supported type or query is models.Document without `model` field
+        """
         if isinstance(query, get_args(types.Query)) or isinstance(query, grpc.Query):
             return query
         if isinstance(query, types.SparseVector):
@@ -738,6 +749,14 @@ class AsyncQdrantFastembedMixin(AsyncQdrantBase):
         raise ValueError(f"Unsupported query type: {type(query)}")
 
     def _resolve_query_request(self, query: models.QueryRequest) -> models.QueryRequest:
+        """Resolve QueryRequest query field
+
+        Args:
+            query: models.QueryRequest - query request to resolve
+
+        Returns:
+            models.QueryRequest: A deepcopy of the query request with resolved query field
+        """
         query = deepcopy(query)
         query.query = self._resolve_query(query.query)
         return query
@@ -745,11 +764,29 @@ class AsyncQdrantFastembedMixin(AsyncQdrantBase):
     def _resolve_query_batch_request(
         self, requests: Sequence[models.QueryRequest]
     ) -> Sequence[models.QueryRequest]:
+        """Resolve query field for each query request in a batch
+
+        Args:
+            requests: Sequence[models.QueryRequest] - query requests to resolve
+
+        Returns:
+            Sequence[models.QueryRequest]: A list of deep copied query requests with resolved query fields
+        """
         return [self._resolve_query_request(query) for query in requests]
 
     def _embed_models(
         self, model: BaseModel, paths: Optional[List[Path]] = None, is_query: bool = False
-    ) -> Union[BaseModel, Union[List[float], models.SparseVector]]:
+    ) -> Union[BaseModel, NumericVector]:
+        """Embed model's fields requiring inference
+
+        Args:
+            model: Qdrant model containing fields to embed
+            paths: Path to fields to embed. E.g. [Path(current="recommend", tail=[Path(current="negative", tail=None)])]
+            is_query: Flag to determine which embed method to use. Defaults to False.
+
+        Returns:
+            A deepcopy of the method with embedded fields
+        """
         if paths is None:
             if isinstance(model, models.Document):
                 return self._embed_raw_data(model, is_query=is_query)
@@ -778,8 +815,17 @@ class AsyncQdrantFastembedMixin(AsyncQdrantBase):
         return model
 
     def _embed_raw_data(
-        self, data: models.VectorInput, is_query: bool = False
-    ) -> NumericVectorInput:
+        self, data: models.VectorStruct, is_query: bool = False
+    ) -> NumericVectorStruct:
+        """Iterates over the data and calls inference on the fields requiring it
+
+        Args:
+            data: models.VectorStruct - data to embed, if it's not a field which requires inference, leave it as is
+            is_query: Flag to determine which embed method to use. Defaults to False.
+
+        Returns:
+            NumericVectorStruct: Embedded data
+        """
         if isinstance(data, models.Document):
             return self._embed_document(data, is_query=is_query)
         elif isinstance(data, dict):
@@ -793,9 +839,19 @@ class AsyncQdrantFastembedMixin(AsyncQdrantBase):
             return [self._embed_raw_data(value, is_query=is_query) for value in data]
         return data
 
-    def _embed_document(
-        self, document: models.Document, is_query: bool = False
-    ) -> Union[List[float], models.SparseVector]:
+    def _embed_document(self, document: models.Document, is_query: bool = False) -> NumericVector:
+        """Embed a document using the specified embedding model
+
+        Args:
+            document: Document to embed
+            is_query: Flag to determine which embed method to use. Defaults to False.
+
+        Returns:
+            NumericVector: Document's embedding
+
+        Raises:
+            ValueError: If model is not supported
+        """
         model_name = document.model
         text = document.text
         if model_name in SUPPORTED_EMBEDDING_MODELS:
