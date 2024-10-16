38060cf036e9eaefc34a2ad89a45e2d4bbc6060f
George Panchuk <george.panchuk@qdrant.tech>
2024-10-16 23:03:27 +0200

    fix: fix exception message

--- qdrant_client/async_qdrant_fastembed.py
+++ qdrant_client/async_qdrant_fastembed.py
@@ -730,9 +730,7 @@ class AsyncQdrantFastembedMixin(AsyncQdrantBase):
         if isinstance(query, models.Document):
             model_name = query.model
             if model_name is None:
-                raise ValueError(
-                    "`query_points` requires explicit model name specification for `Document`"
-                )
+                raise ValueError("`model` field has to be set explicitly in the `Document`")
             return models.NearestQuery(nearest=query)
         if query is None:
             return None
