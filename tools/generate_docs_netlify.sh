e436fd416a484ba926ca7a563d078f20b974e947
George Panchuk <george.panchuk@qdrant.tech>
2024-10-24 15:39:47 +0400

    debug: print virtualenv version

--- tools/generate_docs_netlify.sh
+++ tools/generate_docs_netlify.sh
@@ -8,7 +8,7 @@ cd "$(dirname "$0")/../"
 brew install pandoc
 
 pip install --upgrade "virtualenv>=20.26.6"
-
+pip freeze | grep virtualenv
 curl -sSL https://install.python-poetry.org | python3 -
 export PATH="/opt/buildhome/.local/bin:$PATH"
 poetry install
