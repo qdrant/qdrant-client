accb7aef853abb4a9cddb465a4ff989fa3de0533
George Panchuk <george.panchuk@qdrant.tech>
2024-10-25 03:20:02 +0400

    debug

--- tools/generate_docs_netlify.sh
+++ tools/generate_docs_netlify.sh
@@ -5,15 +5,17 @@ set -x
 # Ensure current path is project root
 cd "$(dirname "$0")/../"
 
+pip install --upgrade "virtualenv==20.26.6"
+python3 -m venv venv
+source venv/bin/activate
+
 brew install pandoc
 
-pip install --upgrade "virtualenv==20.26.6"
-
 export PATH="/opt/buildhome/.local/bin:$PATH"
-curl -sSL https://install.python-poetry.org | python3 -
-error_log=$(find /opt/build/repo/ -name 'poetry-installer-error-*.log' | head -n 1)
-cat "$error_log"
-
+#curl -sSL https://install.python-poetry.org | python3 -
+#error_log=$(find /opt/build/repo/ -name 'poetry-installer-error-*.log' | head -n 1)
+#cat "$error_log"
+pip3 install poetry
 poetry install
 
 sphinx-build docs/source docs/html
