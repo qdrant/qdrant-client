20bc0d41a534c397c903f43bf4c639f1bfbf1e87
Andrey Vasnetsov <andrey@vasnetsov.com>
2024-10-25 15:39:14 +0200

    deprecation warning for grpc.PointStruct

---
+++ qdrant_client/common/deprecations.py
@@ -0,0 +1,22 @@
+import warnings
+from typing import Optional
+
+SEEN_DEPRECATIONS = set()
+
+def deprecation_warning(message: str):
+    warnings.warn(message, DeprecationWarning, stacklevel=2)
+
+
+def deprecation_warning_once(message: str, idx: Optional[str]):
+    """
+    Same as deprecation_warning, but will be shown only once per program run.
+    """
+
+    key = idx if idx is not None else message
+
+    if key not in SEEN_DEPRECATIONS:
+        SEEN_DEPRECATIONS.add(key)
+    else:
+        return
+
+    warnings.warn(message, DeprecationWarning, stacklevel=2)
