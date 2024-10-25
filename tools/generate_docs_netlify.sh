01977782d0d7f930c502d350d41173d1fabcb713
George Panchuk <george.panchuk@qdrant.tech>
2024-10-25 14:41:26 +0200

    debug: remove redundant stmt

--- tools/generate_docs_netlify.sh
+++ tools/generate_docs_netlify.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env sh
 
-set -x
+set -xe
 
 # Ensure current path is project root
 cd "$(dirname "$0")/../"
 
-pip install --upgrade "virtualenv==20.26.6"
 python3 -m venv venv
 source venv/bin/activate
 
 brew install pandoc
 
 export PATH="/opt/buildhome/.local/bin:$PATH"
-#curl -sSL https://install.python-poetry.org | python3 -
-#error_log=$(find /opt/build/repo/ -name 'poetry-installer-error-*.log' | head -n 1)
-#cat "$error_log"
 pip3 install poetry
 poetry install
 
