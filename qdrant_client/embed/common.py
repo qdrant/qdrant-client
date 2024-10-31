75058ad92ca4bf04c8ce3390ae2af64879f7b7a7
George Panchuk <george.panchuk@qdrant.tech>
2024-10-31 17:53:53 +0100

    fix: fix type hints

--- qdrant_client/embed/common.py
+++ qdrant_client/embed/common.py
@@ -1,6 +1,9 @@
-from typing import Set, Tuple
+from typing import Set, Type, Tuple
 
 from qdrant_client.http import models
 
 INFERENCE_OBJECT_NAMES: Set[str] = {"Document", "Image"}
-INFERENCE_OBJECT_TYPES: Tuple = (models.Document, models.Image)
+INFERENCE_OBJECT_TYPES: Tuple[Type[models.Document], Type[models.Image]] = (
+    models.Document,
+    models.Image,
+)
