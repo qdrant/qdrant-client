aa81592d636b3711df25ac0bd8471ec2e9ed9cd4
George <george.panchuk@qdrant.tech>
2024-11-01 22:32:17 +0100

    Custom inference object (#837)
    * new: add inference object support
    
    * new: add inference object support
    
    * fix: remove redundant import
    
    * refactor: return newline
    
    * fix: fix propagate options test

--- qdrant_client/embed/common.py
+++ qdrant_client/embed/common.py
@@ -2,8 +2,7 @@ from typing import Set, Type, Tuple
 
 from qdrant_client.http import models
 
-INFERENCE_OBJECT_NAMES: Set[str] = {"Document", "Image"}
-INFERENCE_OBJECT_TYPES: Tuple[Type[models.Document], Type[models.Image]] = (
-    models.Document,
-    models.Image,
-)
+INFERENCE_OBJECT_NAMES: Set[str] = {"Document", "Image", "InferenceObject"}
+INFERENCE_OBJECT_TYPES: Tuple[
+    Type[models.Document], Type[models.Image], Type[models.InferenceObject]
+] = (models.Document, models.Image, models.InferenceObject)
