39b327f5b8ab7156fdc4d026ce010530e9c9a782
hh-space-invader <h.hagag.ali@gmail.com>
2024-11-15 11:32:37 +0200

    fix: Added missing timeout in rest

--- qdrant_client/qdrant_remote.py
+++ qdrant_client/qdrant_remote.py
@@ -1221,6 +1221,7 @@ class QdrantRemote(QdrantBase):
             http_res: List[List[models.ScoredPoint]] = self.http.search_api.recommend_batch_points(
                 collection_name=collection_name,
                 consistency=consistency,
+                timeout=timeout,
                 recommend_request_batch=models.RecommendRequestBatch(searches=requests),
             ).result
             return http_res
