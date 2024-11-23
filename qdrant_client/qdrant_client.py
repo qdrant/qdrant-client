8ee5b9e2693374fb9de90867ce2a6d87c7d906e4
George Panchuk <george.panchuk@qdrant.tech>
2024-11-23 14:54:10 +0100

    fix: embed dict and list of docs, remove redundant code

--- qdrant_client/qdrant_client.py
+++ qdrant_client/qdrant_client.py
@@ -449,7 +449,7 @@ class QdrantClient(QdrantFastembedMixin):
         requests = self._resolve_query_batch_request(requests)
         requires_inference = self._inference_inspector.inspect(requests)
         if requires_inference and not self.cloud_inference:
-            requests = [self._embed_models(request) for request in requests]
+            requests = self._embed_models(requests)
 
         return self._client.query_batch_points(
             collection_name=collection_name,
@@ -1588,10 +1588,7 @@ class QdrantClient(QdrantFastembedMixin):
         requires_inference = self._inference_inspector.inspect(points)
 
         if requires_inference and not self.cloud_inference:
-            if isinstance(points, list):
-                points = [self._embed_models(point, is_query=False) for point in points]
-            else:
-                points = self._embed_models(points, is_query=False)
+            points = self._embed_models(points, is_query=False)
 
         return self._client.upsert(
             collection_name=collection_name,
@@ -1644,7 +1641,7 @@ class QdrantClient(QdrantFastembedMixin):
 
         requires_inference = self._inference_inspector.inspect(points)
         if requires_inference and not self.cloud_inference:
-            points = [self._embed_models(point, is_query=False) for point in points]
+            points = self._embed_models(points, is_query=False)
 
         return self._client.update_vectors(
             collection_name=collection_name,
@@ -2094,9 +2091,7 @@ class QdrantClient(QdrantFastembedMixin):
         assert len(kwargs) == 0, f"Unknown arguments: {list(kwargs.keys())}"
         requires_inference = self._inference_inspector.inspect(update_operations)
         if requires_inference and not self.cloud_inference:
-            update_operations = [
-                self._embed_models(op, is_query=False) for op in update_operations
-            ]
+            update_operations = self._embed_models(update_operations, is_query=False)
 
         return self._client.batch_update_points(
             collection_name=collection_name,
