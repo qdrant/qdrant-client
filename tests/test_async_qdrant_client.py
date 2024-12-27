3fbdf04f9ea4717c02b3c2b1592aa67216b600b3
Dmitrii Ogn <dimitriy_rudenko@mail.ru>
2024-12-27 16:46:18 +0300

    Removed async_grpc (#866)
    * Removed async_grpc
    
    * Removal of asyncio
    
    * Return needed test
    
    * Proper fix of renaming
    
    ---------
    
    Co-authored-by: d.rudenko <dimitriyrudenk@gmail.com>

--- tests/test_async_qdrant_client.py
+++ tests/test_async_qdrant_client.py
@@ -7,90 +7,14 @@ import numpy as np
 import pytest
 
 import qdrant_client.http.exceptions
-from qdrant_client import QdrantClient
-from qdrant_client import grpc as qdrant_grpc
 from qdrant_client import models
 from qdrant_client.async_qdrant_client import AsyncQdrantClient
-from qdrant_client.conversions.conversion import payload_to_grpc
-from tests.fixtures.payload import one_random_payload_please
 from tests.utils import read_version
 
 NUM_VECTORS = 100
 NUM_QUERIES = 100
 DIM = 32
 COLLECTION_NAME = "async_test_collection"
-
-
-@pytest.mark.asyncio
-async def test_async_grpc():
-    points = (
-        qdrant_grpc.PointStruct(
-            id=qdrant_grpc.PointId(num=idx),
-            vectors=qdrant_grpc.Vectors(
-                vector=qdrant_grpc.Vector(data=np.random.rand(DIM).tolist())
-            ),
-            payload=payload_to_grpc(one_random_payload_please(idx)),
-        )
-        for idx in range(NUM_VECTORS)
-    )
-
-    client = QdrantClient(prefer_grpc=True, timeout=3)
-
-    grpc_collections = client.async_grpc_collections
-
-    res = await grpc_collections.List(qdrant_grpc.ListCollectionsRequest(), timeout=1.0)
-
-    for collection in res.collections:
-        print(collection.name)
-        await grpc_collections.Delete(
-            qdrant_grpc.DeleteCollection(collection_name=collection.name)
-        )
-
-    await grpc_collections.Create(
-        qdrant_grpc.CreateCollection(
-            collection_name=COLLECTION_NAME,
-            vectors_config=qdrant_grpc.VectorsConfig(
-                params=qdrant_grpc.VectorParams(size=DIM, distance=qdrant_grpc.Distance.Cosine)
-            ),
-        )
-    )
-
-    grpc_points = client.async_grpc_points
-
-    upload_features = []
-
-    # Upload vectors in parallel
-    for point in points:
-        upload_features.append(
-            grpc_points.Upsert(
-                qdrant_grpc.UpsertPoints(
-                    collection_name=COLLECTION_NAME, wait=True, points=[point]
-                )
-            )
-        )
-    await asyncio.gather(*upload_features)
-
-    queries = [np.random.rand(DIM).tolist() for _ in range(NUM_QUERIES)]
-
-    # Make async queries
-    search_queries = []
-    for query in queries:
-        search_query = grpc_points.Search(
-            qdrant_grpc.SearchPoints(
-                collection_name=COLLECTION_NAME,
-                vector=query,
-                limit=10,
-            )
-        )
-        search_queries.append(search_query)
-    results = await asyncio.gather(*search_queries)  # All queries are running in parallel now
-
-    assert len(results) == NUM_QUERIES
-
-    for result in results:
-        assert len(result.result) == 10
-
-    client.close()
 
 
 @pytest.mark.asyncio
