6aad04355c11a3c8b3c7f18924562c91dc607395
generall <andrey@vasnetsov.com>
2025-01-03 12:38:51 +0000

    regen async with python 3.10

--- qdrant_client/async_qdrant_remote.py
+++ qdrant_client/async_qdrant_remote.py
@@ -91,7 +91,7 @@ class AsyncQdrantRemote(AsyncQdrantBase):
             if url.startswith("localhost"):
                 url = f"//{url}"
             parsed_url: Url = parse_url(url)
-            self._host, self._port = (parsed_url.host, parsed_url.port)
+            (self._host, self._port) = (parsed_url.host, parsed_url.port)
             if parsed_url.scheme:
                 self._https = parsed_url.scheme == "https"
                 self._scheme = parsed_url.scheme
@@ -198,7 +198,7 @@ class AsyncQdrantRemote(AsyncQdrantBase):
     @staticmethod
     def _parse_url(url: str) -> tuple[Optional[str], str, Optional[int], Optional[str]]:
         parse_result: Url = parse_url(url)
-        scheme, host, port, prefix = (
+        (scheme, host, port, prefix) = (
             parse_result.scheme,
             parse_result.host,
             parse_result.port,
@@ -1708,7 +1708,7 @@ class AsyncQdrantRemote(AsyncQdrantBase):
         **kwargs: Any,
     ) -> types.UpdateResult:
         if self._prefer_grpc:
-            points_selector, opt_shard_key_selector = self._try_argument_to_grpc_selector(points)
+            (points_selector, opt_shard_key_selector) = self._try_argument_to_grpc_selector(points)
             shard_key_selector = shard_key_selector or opt_shard_key_selector
             if isinstance(ordering, models.WriteOrdering):
                 ordering = RestToGrpc.convert_write_ordering(ordering)
@@ -1730,7 +1730,7 @@ class AsyncQdrantRemote(AsyncQdrantBase):
             assert grpc_result is not None, "Delete vectors returned None result"
             return GrpcToRest.convert_update_result(grpc_result)
         else:
-            _points, _filter = self._try_argument_to_rest_points_and_filter(points)
+            (_points, _filter) = self._try_argument_to_rest_points_and_filter(points)
             return (
                 await self.openapi_client.points_api.delete_vectors(
                     collection_name=collection_name,
@@ -1925,7 +1925,7 @@ class AsyncQdrantRemote(AsyncQdrantBase):
         **kwargs: Any,
     ) -> types.UpdateResult:
         if self._prefer_grpc:
-            points_selector, opt_shard_key_selector = self._try_argument_to_grpc_selector(
+            (points_selector, opt_shard_key_selector) = self._try_argument_to_grpc_selector(
                 points_selector
             )
             shard_key_selector = shard_key_selector or opt_shard_key_selector
@@ -1974,7 +1974,7 @@ class AsyncQdrantRemote(AsyncQdrantBase):
         **kwargs: Any,
     ) -> types.UpdateResult:
         if self._prefer_grpc:
-            points_selector, opt_shard_key_selector = self._try_argument_to_grpc_selector(points)
+            (points_selector, opt_shard_key_selector) = self._try_argument_to_grpc_selector(points)
             shard_key_selector = shard_key_selector or opt_shard_key_selector
             if isinstance(ordering, models.WriteOrdering):
                 ordering = RestToGrpc.convert_write_ordering(ordering)
@@ -1997,7 +1997,7 @@ class AsyncQdrantRemote(AsyncQdrantBase):
                 ).result
             )
         else:
-            _points, _filter = self._try_argument_to_rest_points_and_filter(points)
+            (_points, _filter) = self._try_argument_to_rest_points_and_filter(points)
             result: Optional[types.UpdateResult] = (
                 await self.openapi_client.points_api.set_payload(
                     collection_name=collection_name,
@@ -2026,7 +2026,7 @@ class AsyncQdrantRemote(AsyncQdrantBase):
         **kwargs: Any,
     ) -> types.UpdateResult:
         if self._prefer_grpc:
-            points_selector, opt_shard_key_selector = self._try_argument_to_grpc_selector(points)
+            (points_selector, opt_shard_key_selector) = self._try_argument_to_grpc_selector(points)
             shard_key_selector = shard_key_selector or opt_shard_key_selector
             if isinstance(ordering, models.WriteOrdering):
                 ordering = RestToGrpc.convert_write_ordering(ordering)
@@ -2048,7 +2048,7 @@ class AsyncQdrantRemote(AsyncQdrantBase):
                 ).result
             )
         else:
-            _points, _filter = self._try_argument_to_rest_points_and_filter(points)
+            (_points, _filter) = self._try_argument_to_rest_points_and_filter(points)
             result: Optional[types.UpdateResult] = (
                 await self.openapi_client.points_api.overwrite_payload(
                     collection_name=collection_name,
@@ -2076,7 +2076,7 @@ class AsyncQdrantRemote(AsyncQdrantBase):
         **kwargs: Any,
     ) -> types.UpdateResult:
         if self._prefer_grpc:
-            points_selector, opt_shard_key_selector = self._try_argument_to_grpc_selector(points)
+            (points_selector, opt_shard_key_selector) = self._try_argument_to_grpc_selector(points)
             shard_key_selector = shard_key_selector or opt_shard_key_selector
             if isinstance(ordering, models.WriteOrdering):
                 ordering = RestToGrpc.convert_write_ordering(ordering)
@@ -2098,7 +2098,7 @@ class AsyncQdrantRemote(AsyncQdrantBase):
                 ).result
             )
         else:
-            _points, _filter = self._try_argument_to_rest_points_and_filter(points)
+            (_points, _filter) = self._try_argument_to_rest_points_and_filter(points)
             result: Optional[types.UpdateResult] = (
                 await self.openapi_client.points_api.delete_payload(
                     collection_name=collection_name,
@@ -2122,7 +2122,7 @@ class AsyncQdrantRemote(AsyncQdrantBase):
         **kwargs: Any,
     ) -> types.UpdateResult:
         if self._prefer_grpc:
-            points_selector, opt_shard_key_selector = self._try_argument_to_grpc_selector(
+            (points_selector, opt_shard_key_selector) = self._try_argument_to_grpc_selector(
                 points_selector
             )
             shard_key_selector = shard_key_selector or opt_shard_key_selector
