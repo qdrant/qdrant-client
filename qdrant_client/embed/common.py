25fa5c54ab02d649803081843e9911ada01c1ac7
George Panchuk <george.panchuk@qdrant.tech>
2025-01-14 18:17:58 +0100

    fix: remove redundant code

--- qdrant_client/embed/common.py
+++ qdrant_client/embed/common.py
@@ -3,7 +3,4 @@ from typing import Type, Union
 from qdrant_client.http import models
 
 INFERENCE_OBJECT_NAMES: set[str] = {"Document", "Image", "InferenceObject"}
-# INFERENCE_OBJECT_TYPES_TUPLE: tuple[
-#     Type[models.Document], Type[models.Image], Type[models.InferenceObject]
-# ] = (models.Document, models.Image, models.InferenceObject)
 INFERENCE_OBJECT_TYPES = Union[models.Document, models.Image, models.InferenceObject]
