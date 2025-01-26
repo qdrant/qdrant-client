1cf81ed18a3eb0ed344a95a0cb4851f5036cd6a5
George Panchuk <george.panchuk@qdrant.tech>
2025-01-26 14:30:44 +0100

    new: extend embedding size to support image and late interaction models

--- tests/test_fastembed.py
+++ tests/test_fastembed.py
@@ -201,3 +201,21 @@ def test_idf_models():
 
     # the only sparse model without IDF is SPLADE, however it's too large for tests, so we don't test how non-idf
     # models work
+
+
+def test_get_embedding_size():
+    local_client = QdrantClient(":memory:")
+
+    if not local_client._FASTEMBED_INSTALLED:
+        pytest.skip("FastEmbed is not installed, skipping test")
+
+    assert local_client.get_embedding_size() == 384
+
+    assert local_client.get_embedding_size(model_name="BAAI/bge-base-en-v1.5") == 768
+
+    assert local_client.get_embedding_size(model_name="Qdrant/resnet50-onnx") == 2048
+
+    assert local_client.get_embedding_size(model_name="colbert-ir/colbertv2.0") == 128
+
+    with pytest.raises(ValueError, match="Sparse embeddings do not have a fixed embedding size."):
+        local_client.get_embedding_size(model_name="Qdrant/bm25")
