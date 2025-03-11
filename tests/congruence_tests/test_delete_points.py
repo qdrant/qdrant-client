4d4c6a6846861483427c71e9fb8903dc5892d2ef
hh-space-invader <h.hagag.ali@gmail.com>
2025-03-11 10:18:00 +0200

    improve: Improved test_delete_points

--- tests/congruence_tests/test_delete_points.py
+++ tests/congruence_tests/test_delete_points.py
@@ -1,3 +1,6 @@
+import uuid
+
+from qdrant_client.client_base import QdrantBase
 from qdrant_client.http.models import NamedSparseVector, NamedVector
 from tests.congruence_tests.test_common import (
     COLLECTION_NAME,
@@ -6,92 +9,99 @@ from tests.congruence_tests.test_common import (
     generate_fixtures,
     generate_sparse_fixtures,
     init_client,
-    init_local,
-    init_remote,
     sparse_vectors_config,
+    initialize_fixture_collection,
 )
 
 
-def test_delete_points(local_client, remote_client):
+def test_delete_points(local_client: QdrantBase, remote_client: QdrantBase):
     points = generate_fixtures(100)
     vector = points[0].vector["image"]
-    local_client.upload_points(COLLECTION_NAME, points)
-    remote_client.upload_points(COLLECTION_NAME, points, wait=True)
+
+    collection_name = f"{COLLECTION_NAME}_{uuid.uuid4().hex}"
+    initialize_fixture_collection(local_client, collection_name)
+    initialize_fixture_collection(remote_client, collection_name)
+    local_client.upload_points(collection_name, points)
+    remote_client.upload_points(collection_name, points, wait=True)
 
     compare_client_results(
         local_client,
         remote_client,
-        lambda c: c.search(COLLECTION_NAME, query_vector=NamedVector(name="image", vector=vector)),
+        lambda c: c.search(collection_name, query_vector=NamedVector(name="image", vector=vector)),
     )
 
     found_ids = [
         scored_point.id
         for scored_point in local_client.search(
-            COLLECTION_NAME, query_vector=NamedVector(name="image", vector=vector)
+            collection_name, query_vector=NamedVector(name="image", vector=vector)
         )
     ]
 
-    local_client.delete(COLLECTION_NAME, found_ids)
-    remote_client.delete(COLLECTION_NAME, found_ids)
+    local_client.delete(collection_name, found_ids)
+    remote_client.delete(collection_name, found_ids)
 
-    compare_collections(local_client, remote_client, 100, attrs=("points_count",))
+    compare_collections(
+        local_client, remote_client, 100, attrs=("points_count",), collection_name=collection_name
+    )
 
     compare_client_results(
         local_client,
         remote_client,
-        lambda c: c.search(COLLECTION_NAME, query_vector=NamedVector(name="image", vector=vector)),
+        lambda c: c.search(collection_name, query_vector=NamedVector(name="image", vector=vector)),
     )
 
-    #delete non-existent points
-    local_client.delete(COLLECTION_NAME, found_ids)
-    remote_client.delete(COLLECTION_NAME, found_ids)
+    # delete non-existent points
+    local_client.delete(collection_name, found_ids)
+    remote_client.delete(collection_name, found_ids)
 
-    compare_collections(local_client, remote_client, 100, attrs=("points_count",))
+    compare_collections(
+        local_client, remote_client, 100, attrs=("points_count",), collection_name=collection_name
+    )
 
     compare_client_results(
         local_client,
         remote_client,
-        lambda c: c.search(COLLECTION_NAME, query_vector=NamedVector(name="image", vector=vector)),
+        lambda c: c.search(collection_name, query_vector=NamedVector(name="image", vector=vector)),
     )
 
 
-def test_delete_sparse_points():
+def test_delete_sparse_points(local_client: QdrantBase, remote_client: QdrantBase):
     points = generate_sparse_fixtures(100)
     vector = points[0].vector["sparse-image"]
 
-    local_client = init_local()
-    init_client(local_client, [], sparse_vectors_config=sparse_vectors_config)
+    collection_name = f"{COLLECTION_NAME}_{uuid.uuid4().hex}"
+    init_client(local_client, [], collection_name, sparse_vectors_config=sparse_vectors_config)
+    init_client(remote_client, [], collection_name, sparse_vectors_config=sparse_vectors_config)
 
-    remote_client = init_remote()
-    init_client(remote_client, [], sparse_vectors_config=sparse_vectors_config)
-
-    local_client.upload_points(COLLECTION_NAME, points)
-    remote_client.upload_points(COLLECTION_NAME, points, wait=True)
+    local_client.upload_points(collection_name, points)
+    remote_client.upload_points(collection_name, points, wait=True)
 
     compare_client_results(
         local_client,
         remote_client,
         lambda c: c.search(
-            COLLECTION_NAME, query_vector=NamedSparseVector(name="sparse-image", vector=vector)
+            collection_name, query_vector=NamedSparseVector(name="sparse-image", vector=vector)
         ),
     )
 
     found_ids = [
         scored_point.id
         for scored_point in local_client.search(
-            COLLECTION_NAME, query_vector=NamedSparseVector(name="sparse-image", vector=vector)
+            collection_name, query_vector=NamedSparseVector(name="sparse-image", vector=vector)
         )
     ]
 
-    local_client.delete(COLLECTION_NAME, found_ids)
-    remote_client.delete(COLLECTION_NAME, found_ids)
+    local_client.delete(collection_name, found_ids)
+    remote_client.delete(collection_name, found_ids)
 
-    compare_collections(local_client, remote_client, 100, attrs=("points_count",))
+    compare_collections(
+        local_client, remote_client, 100, attrs=("points_count",), collection_name=collection_name
+    )
 
     compare_client_results(
         local_client,
         remote_client,
         lambda c: c.search(
-            COLLECTION_NAME, query_vector=NamedSparseVector(name="sparse-image", vector=vector)
+            collection_name, query_vector=NamedSparseVector(name="sparse-image", vector=vector)
         ),
     )
