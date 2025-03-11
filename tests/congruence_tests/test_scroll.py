379934ab1f7b23c19978f16c73ef2e395e79e269
hh-space-invader <h.hagag.ali@gmail.com>
2025-03-11 12:31:30 +0200

    improve: Improved test_scroll

--- tests/congruence_tests/test_scroll.py
+++ tests/congruence_tests/test_scroll.py
@@ -16,11 +16,13 @@ from tests.congruence_tests.test_common import (
 
 class TestSimpleScroller:
     @classmethod
-    def scroll_all(cls, client: QdrantBase) -> list[models.Record]:
+    def scroll_all(
+        cls, client: QdrantBase, collection_name: str = COLLECTION_NAME
+    ) -> list[models.Record]:
         all_records = []
 
         records, next_page = client.scroll(
-            collection_name=COLLECTION_NAME,
+            collection_name=collection_name,
             limit=10,
             with_payload=True,
         )
@@ -28,7 +30,7 @@ class TestSimpleScroller:
 
         while next_page:
             records, next_page = client.scroll(
-                collection_name=COLLECTION_NAME,
+                collection_name=collection_name,
                 limit=20,
                 offset=next_page,
                 with_payload=True,
@@ -38,35 +40,43 @@ class TestSimpleScroller:
         return all_records
 
 
-def test_simple_search() -> None:
+def test_simple_search(collection_name: str) -> None:
     fixture_points = generate_fixtures(200)
 
     scroller = TestSimpleScroller()
 
     local_client = init_local()
-    init_client(local_client, fixture_points)
+    init_client(local_client, fixture_points, collection_name)
 
     remote_client = init_remote()
-    init_client(remote_client, fixture_points)
+    init_client(remote_client, fixture_points, collection_name)
 
-    compare_client_results(local_client, remote_client, scroller.scroll_all)
+    compare_client_results(
+        local_client, remote_client, scroller.scroll_all, collection_name=collection_name
+    )
 
 
-def test_simple_sparse_scroll() -> None:
+def test_simple_sparse_scroll(collection_name: str) -> None:
     fixture_points = generate_sparse_fixtures(200)
 
     local_client = init_local()
-    init_client(local_client, fixture_points, sparse_vectors_config=sparse_vectors_config)
+    init_client(
+        local_client, fixture_points, collection_name, sparse_vectors_config=sparse_vectors_config
+    )
 
     remote_client = init_remote()
-    init_client(remote_client, fixture_points, sparse_vectors_config=sparse_vectors_config)
+    init_client(
+        remote_client, fixture_points, collection_name, sparse_vectors_config=sparse_vectors_config
+    )
 
     scroller = TestSimpleScroller()
 
-    compare_client_results(local_client, remote_client, scroller.scroll_all)
+    compare_client_results(
+        local_client, remote_client, scroller.scroll_all, collection_name=collection_name
+    )
 
 
-def test_mixed_ids() -> None:
+def test_mixed_ids(collection_name: str) -> None:
     fixture_points = generate_fixtures(100, random_ids=True) + generate_fixtures(
         100, random_ids=False
     )
@@ -76,15 +86,17 @@ def test_mixed_ids() -> None:
     scroller = TestSimpleScroller()
 
     local_client = init_local()
-    init_client(local_client, fixture_points)
+    init_client(local_client, fixture_points, collection_name)
 
     remote_client = init_remote()
-    init_client(remote_client, fixture_points)
+    init_client(remote_client, fixture_points, collection_name)
 
-    compare_client_results(local_client, remote_client, scroller.scroll_all)
+    compare_client_results(
+        local_client, remote_client, scroller.scroll_all, collection_name=collection_name
+    )
 
 
-def test_sparse_mixed_ids() -> None:
+def test_sparse_mixed_ids(collection_name: str) -> None:
     fixture_points = generate_sparse_fixtures(100, random_ids=True) + generate_sparse_fixtures(
         100, random_ids=False
     )
@@ -94,9 +106,15 @@ def test_sparse_mixed_ids() -> None:
     scroller = TestSimpleScroller()
 
     local_client = init_local()
-    init_client(local_client, fixture_points, sparse_vectors_config=sparse_vectors_config)
+    init_client(
+        local_client, fixture_points, collection_name, sparse_vectors_config=sparse_vectors_config
+    )
 
     remote_client = init_remote()
-    init_client(remote_client, fixture_points, sparse_vectors_config=sparse_vectors_config)
+    init_client(
+        remote_client, fixture_points, collection_name, sparse_vectors_config=sparse_vectors_config
+    )
 
-    compare_client_results(local_client, remote_client, scroller.scroll_all)
+    compare_client_results(
+        local_client, remote_client, scroller.scroll_all, collection_name=collection_name
+    )
