b247666a3b0517369771d8a933ed807d7cfb02a6
hh-space-invader <h.hagag.ali@gmail.com>
2025-03-11 10:09:53 +0200

    improve: Improved test_count

--- tests/congruence_tests/test_count.py
+++ tests/congruence_tests/test_count.py
@@ -1,3 +1,5 @@
+import uuid
+
 from qdrant_client.client_base import QdrantBase
 from qdrant_client.http import models
 from tests.congruence_tests.test_common import (
@@ -13,58 +15,68 @@ from tests.congruence_tests.test_common import (
 from tests.fixtures.filters import one_random_filter_please
 
 
-def count_all(client: QdrantBase) -> int:
+def count_all(client: QdrantBase, collection_name: str = COLLECTION_NAME) -> int:
     return client.count(
-        collection_name=COLLECTION_NAME,
+        collection_name=collection_name,
         count_filter=None,
     ).count
 
 
-def filter_count(client: QdrantBase, count_filter: models.Filter) -> int:
+def filter_count(
+    client: QdrantBase, count_filter: models.Filter, collection_name: str = COLLECTION_NAME
+) -> int:
     return client.count(
-        collection_name=COLLECTION_NAME,
+        collection_name=collection_name,
         count_filter=count_filter,
     ).count
 
 
-def test_simple_count():
+def test_simple_count(local_client: QdrantBase, remote_client: QdrantBase):
     fixture_points = generate_fixtures()
 
-    local_client = init_local()
-    init_client(local_client, fixture_points)
+    collection_name = f"{COLLECTION_NAME}_{uuid.uuid4().hex}"
+    init_client(local_client, fixture_points, collection_name)
+    init_client(remote_client, fixture_points, collection_name)
 
-    remote_client = init_remote()
-    init_client(remote_client, fixture_points)
+    compare_client_results(local_client, remote_client, count_all, collection_name=collection_name)
 
-    compare_client_results(local_client, remote_client, count_all)
-
-    for i in range(100):
+    for _ in range(100):
         count_filter = one_random_filter_please()
         try:
             compare_client_results(
-                local_client, remote_client, filter_count, count_filter=count_filter
+                local_client,
+                remote_client,
+                filter_count,
+                count_filter=count_filter,
+                collection_name=collection_name,
             )
         except AssertionError as e:
             print(f"\nFailed with filter {count_filter}")
             raise e
 
 
-def test_simple_sparse_search():
+def test_simple_sparse_search(local_client: QdrantBase, remote_client: QdrantBase):
     fixture_points = generate_sparse_fixtures()
 
-    local_client = init_local()
-    init_client(local_client, fixture_points, sparse_vectors_config=sparse_vectors_config)
+    collection_name = f"{COLLECTION_NAME}_{uuid.uuid4().hex}"
+    init_client(
+        local_client, fixture_points, collection_name, sparse_vectors_config=sparse_vectors_config
+    )
+    init_client(
+        remote_client, fixture_points, collection_name, sparse_vectors_config=sparse_vectors_config
+    )
 
-    remote_client = init_remote()
-    init_client(remote_client, fixture_points, sparse_vectors_config=sparse_vectors_config)
+    compare_client_results(local_client, remote_client, count_all, collection_name=collection_name)
 
-    compare_client_results(local_client, remote_client, count_all)
-
-    for i in range(100):
+    for _ in range(100):
         count_filter = one_random_filter_please()
         try:
             compare_client_results(
-                local_client, remote_client, filter_count, count_filter=count_filter
+                local_client,
+                remote_client,
+                filter_count,
+                count_filter=count_filter,
+                collection_name=collection_name,
             )
         except AssertionError as e:
             print(f"\nFailed with filter {count_filter}")
