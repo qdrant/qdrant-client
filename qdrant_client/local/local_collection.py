new: recommend average multivectors, fix tests, add new tests for multivectors

qdrant_client/local/local_collection.py
@@ -553,7 +553,6 @@ class LocalCollection:
         score_threshold: Optional[float] = None,
     ) -> list[models.ScoredPoint]:
         name, query_vector = self._resolve_query_vector_name(query_vector)
-
         result: list[models.ScoredPoint] = []
         sparse_scoring = False
         rescore_idf = False
@@ -581,6 +580,7 @@ class LocalCollection:
             distance = self.get_vector_params(name).distance
 
         vectors = vectors[: len(self.payload)]
+
         if isinstance(query_vector, np.ndarray):
             if len(query_vector.shape) == 1:
                 scores = calculate_distance(query_vector, vectors, distance)
@@ -1356,6 +1356,18 @@ class LocalCollection:
             vector = mean_positive_vector
         return vector
 
+    @staticmethod
+    def _recommend_average_multi(
+        positive_vectors: list[list[list[float]]], negative_vectors: list[list[list[float]]]
+    ) -> list[list[float]]:
+        recommend_vector = [vector for multi_vector in positive_vectors for vector in multi_vector]
+        if len(negative_vectors) > 0:
+            for multi_vector in negative_vectors:
+                for vector in multi_vector:
+                    recommend_vector.append([-value for value in vector])
+
+        return recommend_vector
+
     def _construct_recommend_query(
         self,
         positive: Optional[Sequence[models.VectorInput]] = None,
@@ -1402,9 +1414,8 @@ class LocalCollection:
                     sparse_negative_vectors,
                 )
             elif multi_positive_vectors:
-                raise TypeError(
-                    "Multivectors do not support recommend average, consider using "
-                    "strategy=models.RecommendStrategy.BEST_SCORE"
+                query_vector = self._recommend_average_multi(
+                    multi_positive_vectors, multi_negative_vectors
                 )
             else:
                 raise ValueError("No positive examples given with 'average_vector' strategy")
@@ -1735,7 +1746,6 @@ class LocalCollection:
             raise ValueError(
                 "All context example pairs must be either dense or sparse or multi vectors"
             )
-
         return dense_context_vectors, sparse_context_vectors, multi_context_vectors, mentioned_ids
 
     def _preprocess_discover(
@@ -1839,7 +1849,6 @@ class LocalCollection:
             raise ValueError("No target or context given")
 
         search_in_vector_name = using if using is not None else DEFAULT_VECTOR_NAME
-
         return self.search(
             query_vector=(search_in_vector_name, query_vector),
             query_filter=edited_query_filter,

tests/congruence_tests/test_multivector_recommend_queries.py
@@ -1,4 +1,5 @@
-import numpy as np
+from typing import Optional
+
 
 from qdrant_client.client_base import QdrantBase
 from qdrant_client.http.models import models
@@ -7,7 +8,6 @@ from tests.congruence_tests.test_common import (
     COLLECTION_NAME,
     compare_client_results,
     generate_multivector_fixtures,
-    image_vector_size,
     init_client,
     init_local,
     init_remote,
@@ -20,8 +20,70 @@ secondary_collection_name = "congruence_secondary_collection"
 class TestSimpleRecommendation:
     __test__ = False
 
-    def __init__(self):
-        self.query_image = np.random.random(image_vector_size).tolist()
+    @classmethod
+    def simple_recommend_image(cls, client: QdrantBase) -> list[models.ScoredPoint]:
+        return client.query_points(
+            collection_name=COLLECTION_NAME,
+            query=models.RecommendQuery(
+                recommend=models.RecommendInput(
+                    positive=[10],
+                    negative=[],
+                )
+            ),
+            with_payload=True,
+            limit=10,
+            using="multi-image",
+        ).points
+
+    @classmethod
+    def many_recommend(cls, client: QdrantBase) -> list[models.ScoredPoint]:
+        return client.query_points(
+            collection_name=COLLECTION_NAME,
+            query=models.RecommendQuery(
+                recommend=models.RecommendInput(
+                    positive=[10, 19],
+                )
+            ),
+            with_payload=True,
+            limit=10,
+            using="multi-text",
+        ).points
+
+    @classmethod
+    def simple_recommend_negative(cls, client: QdrantBase) -> list[models.ScoredPoint]:
+        return client.query_points(
+            collection_name=COLLECTION_NAME,
+            query=models.RecommendQuery(
+                recommend=models.RecommendInput(
+                    positive=[10],
+                    negative=[15, 7],
+                )
+            ),
+            with_payload=True,
+            limit=10,
+            using="multi-image",
+        ).points
+
+    @classmethod
+    def recommend_from_another_collection(
+        cls, client: QdrantBase, positive_point_id: Optional[int] = None
+    ) -> list[models.ScoredPoint]:
+        return client.query_points(
+            collection_name=COLLECTION_NAME,
+            query=models.RecommendQuery(
+                recommend=models.RecommendInput(
+                    positive=[10] if positive_point_id is None else [positive_point_id],
+                    negative=[15, 7] if positive_point_id is None else [],
+                )
+            ),
+            with_payload=True,
+            limit=10,
+            using="multi-image",
+            lookup_from=models.LookupLocation(
+                collection=secondary_collection_name,
+                vector="multi-image",
+            ),
+        ).points
 
     @classmethod
     def best_score_recommend(cls, client: QdrantBase) -> list[models.ScoredPoint]:
@@ -166,7 +228,7 @@ class TestSimpleRecommendation:
                             recommend=models.RecommendInput(
                                 positive=[10],
                                 negative=[],
-                                strategy=models.RecommendStrategy.BEST_SCORE,
+                                strategy=models.RecommendStrategy.AVERAGE_VECTOR,
                             )
                         ),
                         limit=2,
@@ -179,13 +241,13 @@ class TestSimpleRecommendation:
                     models.QueryRequest(
                         query=models.RecommendQuery(
                             recommend=models.RecommendInput(
-                                positive=[4],
+                                positive=[3],
                                 negative=[],
-                                strategy=models.RecommendStrategy.SUM_SCORES,
+                                strategy=models.RecommendStrategy.BEST_SCORE,
                             )
                         ),
                         limit=2,
-                        using="multi-image",
+                        using="multi-code",
                     ),
                 ],
             )
@@ -216,7 +278,10 @@ def test_simple_recommend() -> None:
         secondary_collection_name,
         vectors_config=multi_vector_config,
     )
-
+    compare_client_results(local_client, remote_client, searcher.simple_recommend_image)
+    compare_client_results(local_client, remote_client, searcher.many_recommend)
+    compare_client_results(local_client, remote_client, searcher.simple_recommend_negative)
+    compare_client_results(local_client, remote_client, searcher.recommend_from_another_collection)
     compare_client_results(local_client, remote_client, searcher.best_score_recommend)
     compare_client_results(local_client, remote_client, searcher.best_score_recommend_euclid)
     compare_client_results(