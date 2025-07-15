952e55929299c0a326e3e77696e76fa759d51613
George <george.panchuk@qdrant.tech>
2025-07-15 18:41:58 +0300

    new: add b64 encoding function (#1031)
    * new: add b64 encoding function
    
    * tests: remove garbage
    
    * refactor base64 function name and params
    
    * manual test for remove iamge inference with base64
    
    * use tempfile
    
    * fix: rename remove_inference->remote_inference
    
    ---------
    
    Co-authored-by: generall <andrey@vasnetsov.com>

---
+++ tests/test_remote_inference.py
@@ -0,0 +1,63 @@
+import pytest
+import os
+import httpx
+import tempfile
+
+from qdrant_client import QdrantClient, models
+from qdrant_client.embed.utils import read_base64
+
+
+QDRANT_URL: str = os.getenv("QDRANT_URL", "http://localhost:6333")
+QDRANT_API_KEY: str = os.getenv("QDRANT_API_KEY", "")
+
+
+# This test requires configured remote inference server, so it is disabled by default and
+# expected to be used manually.
+@pytest.mark.skip(reason="Requires configured remote inference server")
+def test_remote_inference_image():
+    client = QdrantClient(url=QDRANT_URL, api_key=QDRANT_API_KEY, cloud_inference=True)
+    collection_name = "image_embeddings"
+    model_name = "Qdrant/clip-ViT-B-32-vision"
+
+    image_url = "https://qdrant.tech/example.png"
+
+    # Compare inference of image exposed via url and local file
+    # So download image to local file and compare results
+
+    with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as tmp_file:
+        image_path = tmp_file.name
+
+    with httpx.Client() as httpx_client:
+        response = httpx_client.get(image_url)
+        with open(image_path, "wb") as f:
+            f.write(response.content)
+
+    if client.collection_exists(collection_name):
+        client.delete_collection(collection_name)
+
+    client.create_collection(
+        collection_name=collection_name,
+        vectors_config=models.VectorParams(
+            size=client.get_embedding_size(model_name), distance=models.Distance.COSINE
+        ),
+    )
+
+    client.upsert(
+        collection_name=collection_name,
+        points=[
+            models.PointStruct(
+                id=1,
+                vector=models.Image(
+                    image=image_url,
+                    model=model_name,
+                ),
+            ),
+            models.PointStruct(
+                id=2,
+                vector=models.Image(
+                    image=read_base64(image_path),
+                    model=model_name,
+                ),
+            ),
+        ],
+    )
