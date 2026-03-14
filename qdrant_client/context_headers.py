e945a41b3cc361df5049e80ba4c7f40dae7b47af
generall <andrey@vasnetsov.com>
2026-03-15 00:50:31 +0100

    mypy

--- qdrant_client/context_headers.py
+++ qdrant_client/context_headers.py
@@ -1,6 +1,8 @@
 from contextlib import asynccontextmanager, contextmanager
 from contextvars import ContextVar
-from typing import AsyncIterator, Iterator
+from typing import AsyncIterator, Awaitable, Callable, Iterator
+
+from httpx import Request, Response
 
 _context_headers: ContextVar[dict[str, str]] = ContextVar("_context_headers", default={})
 
@@ -31,13 +33,15 @@ async def async_headers(extra_headers: dict[str, str]) -> AsyncIterator[None]:
         _context_headers.reset(token)
 
 
-def rest_headers_middleware(request, call_next):
+def rest_headers_middleware(request: Request, call_next: Callable[[Request], Response]) -> Response:
     for key, value in get_context_headers().items():
         request.headers[key] = value
     return call_next(request)
 
 
-async def async_rest_headers_middleware(request, call_next):
+async def async_rest_headers_middleware(
+    request: Request, call_next: Callable[[Request], Awaitable[Response]]
+) -> Response:
     for key, value in get_context_headers().items():
         request.headers[key] = value
     return await call_next(request)
