20cc4e19e9f8622225dff7cc9bd97c20e39429ef
George <george.panchuk@qdrant.tech>
2026-09-04 00:35:45 +0700

    fix: fix missing multivector placeholder in local mode (#1399)
    * fix: fix missing multivector placeholder in local mode
    
    * fix: do not use deleted vectors in recommend, etc
    
    * fix: fix mypy complaints in point id vector resolution
    
    * fix: regen async

--- qdrant_client/local/qdrant_local.py
+++ qdrant_client/local/qdrant_local.py
@@ -257,15 +257,6 @@ class QdrantLocal(QdrantBase):
             else search_in_vector_name
         )
 
-        sparse = vector_name in collection.sparse_vectors
-        multi = vector_name in collection.multivectors
-        if sparse:
-            collection_vectors = collection.sparse_vectors
-        elif multi:
-            collection_vectors = collection.multivectors
-        else:
-            collection_vectors = collection.vectors
-
         # mentioned ids in the search collection which should be excluded from search
         mentioned_ids: set[types.PointId] = set()
 
@@ -276,16 +267,7 @@ class QdrantLocal(QdrantBase):
                 if isinstance(vector_input, uuid.UUID):
                     vector_input = str(vector_input)
                 point_id = vector_input  # rename for clarity
-                if point_id not in collection.ids:
-                    raise ValueError(f"Point {point_id} is not found in the collection")
-
-                idx = collection.ids[point_id]
-                if vector_name in collection_vectors:
-                    vec = collection_vectors[vector_name][idx]
-                else:
-                    raise ValueError(f"Vector {vector_name} not found")
-                if isinstance(vec, np.ndarray):
-                    vec = vec.tolist()
+                vec = collection._vector_by_point_id(vector_name, point_id)
                 if collection_name == lookup_collection_name:
                     mentioned_ids.add(point_id)
                 return vec
